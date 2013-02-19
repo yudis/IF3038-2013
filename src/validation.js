504fcb252f705c39d81f5284da5b2cce18e24019
Ikmal Syifai <ikmal.s.16@gmail.com>
2013-02-19 13:14:26 +0700

    First commit

---
+++ src/validation.js
@@ -0,0 +1,48 @@
+function addEvent(node, type, callback){
+	if(node.addEventListener){
+		node.addEventListener(type, function(e){
+			callback(e, e.target);
+			
+		}, false);
+	}
+	else if(node.attachEvent){
+		node.attachEvent('on' + type, function(e){
+			callback(e, e.srcElement);
+		});
+	}
+}
+
+
+function shouldBeValidated(field){
+	return (
+		!(field.getAttribute('readonly') || field.readonly)	&&
+		!(field.getAttribute('disabled') || field.disabled)	&&
+		(field.getAttribute('pattern') || field.getAttribute('required'))
+	); 
+}
+
+
+function instantValidation(field){
+	if(shouldBeValidated(field)){
+		var invalid = (
+			(field.getAttribute('required') && !field.value) ||
+			(field.getAttribute('pattern') && field.value && !new RegExp(field.getAttribute('pattern')).test(field.value))
+		);
+
+		if(!invalid && field.getAttribute('aria-invalid')){
+			field.removeAttribute('aria-invalid');
+		}
+		else if(invalid && !field.getAttribute('aria-invalid')){
+			field.setAttribute('aria-invalid', 'true');
+		}
+	}
+}
+
+var fields = [document.getElementsByTagName('input'), document.getElementsByTagName('textarea')];
+for(var a = fields.length, i = 0; i < a; i ++){
+	for(var b = fields[i].length, j = 0; j < b; j ++){
+		addEvent(fields[i][j], 'change', function(e, target){
+			instantValidation(target);
+		});
+	}
+}
