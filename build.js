258ff57ec587ee822f63244c830264c7df5407b0
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-02-19 10:51:39 +0700

    Implemented a build framework using NodeJS
    Use command: node build.js to build the project using the templates in
    src/.
    The built files will be placed into build/.

---
+++ build.js
@@ -0,0 +1,36 @@
+// settings
+var FILE_ENCODING = 'utf-8',
+    EOL = '\n';
+ 
+// setup
+var _fs = require('fs');
+ 
+function concat(opts) {
+    var fileList = opts.src;
+    var distPath = opts.dest;
+    var title = opts.title;
+    var out = fileList.map(function(filePath){
+            src = _fs.readFileSync(filePath, FILE_ENCODING);
+            src = src.replace('{{ title }}', title);
+            return src;
+        });
+
+    _fs.writeFileSync(distPath, out.join(EOL), FILE_ENCODING);
+    console.log(' '+ distPath +' built.');
+}
+
+var files = ['login', 'new_tugas', 'profile', 'register', 'dashboard'];
+var titles = ['Login', 'Tugas Baru', 'Profil', 'Pendaftaran', 'Dashboard'];
+
+files.forEach(function(v, k) {
+    var title = titles[k];
+    concat({
+        src : [
+            'src/header.html',
+            'src/' + v + '.html',
+            'src/footer.html'
+        ],
+        dest : 'build/' + v + '.html',
+        title : title
+    });
+});
