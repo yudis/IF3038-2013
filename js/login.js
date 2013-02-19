09264cbfa517903e78c523eda3a0efd705ca4498
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2013-02-19 17:37:47 +0700

    a more convenient register form

--- js/login.js
+++ js/login.js
@@ -6,10 +6,51 @@
 })(window);
 
 window.onload = function() {
-	$id("loginform-submit").disabled = true;
-	$id("registerform-submit").disabled = true;
+	window.registerform = {
+		username: $id("registerform-username"),
+		password: $id("registerform-password"),
+		password2: $id("registerform-password2"),
+		email: $id("registerform-email"),
+		fullname: $id("registerform-fullname"),
+		birthdate: $id("registerform-birthdate"),
+		avatar: $id("registerform-avatar"),
+		//equalsUPE: $id("registerform-equalsUPE")
+	}
+	window.error_message = {
+		username: 'Minimal 5 characters, only alphabetic, numeric and underscore allowed',
+		password: 'Minimal 8 characters',
+		password2: 'Must same with password',
+		email: 'Standard email format',
+		fullname: 'At least contains firstname and lastname',
+		birthdate: 'Incorrect date format',
+		avatar: 'Format must be .jpg',
+		equalsUPE: 'Password cannot be same with Username and Email'
+	}
+	window.registerform_validation = {};
 	register_check();
 	login_check();
+
+	for(key in window.registerform) {
+		window.registerform[key].onkeyup = function(e) {
+			register_check();
+			var key = $id(this.id).id.split('-')[1];
+			console.log(key);
+			
+			if(!window.registerform_validation[key]) {
+				if(!hasClass(window.registerform[key], 'error')) {
+					var e = document.createElement('div');
+					addClass(e, 'errorMessage');
+					e.innerHTML = error_message[key];
+			
+					addClass(window.registerform[key], 'error');
+					window.registerform[key].parentNode.appendChild(e);
+				}
+			} else if(hasClass(window.registerform[key], 'error')) {
+				deleteClass(window.registerform[key], 'error');
+				window.registerform[key].parentNode.removeChild(window.registerform[key].parentNode.lastChild);
+			}
+		}
+	}
 }
 
 function login_check() {
@@ -30,63 +71,31 @@ function register_check() {
 	var regex_fullname = /^[A-Za-z]+ [A-Za-z \.]+$/;
 	var regex_birthdate = /^\d{4}\-\d{2}\-\d{2}$/;
 	var avatar_ext = $id("registerform-avatar").value.split('.').pop();
-	var registerform_validation = {};
-	var ok = true;
 	
-	var registerform = {
-		username: $id("registerform-username"),
-		password: $id("registerform-password"),
-		password2: $id("registerform-password2"),
-		email: $id("registerform-email"),
-		fullname: $id("registerform-fullname"),
-		birthdate: $id("registerform-birthdate"),
-		avatar: $id("registerform-avatar"),
-		equalsUPE: $id("registerform-equalsUPE")
-	}
+	var ok = true;
 
-	var error_message = {
-		username: 'Minimal 5 characters, only alphabetic, numeric and underscore allowed',
-		password: 'Minimal 8 characters',
-		password2: 'Must same with password',
-		email: 'Standard email format',
-		fullname: 'At least contains firstname and lastname',
-		birthdate: 'Incorrect date format',
-		avatar: 'Format must be .jpg',
-		equalsUPE: 'Password cannot be same with Username and Email'
-	}
-
-	registerform_validation['username'] = regex_username.test($id("registerform-username").value);
-	registerform_validation['password'] = regex_password.test($id("registerform-password").value);
-	registerform_validation['equalsUPE'] = ($id("registerform-password").value != $id("registerform-username").value) && ($id("registerform-password").value != $id("registerform-email").value);
-	registerform_validation['password2'] = ($id("registerform-password").value == $id("registerform-password2").value);
-	registerform_validation['fullname'] = regex_fullname.test($id("registerform-fullname").value);
-	registerform_validation['birthdate'] = (regex_birthdate.test($id("registerform-birthdate").value)) && (parseInt($id("registerform-birthdate").value.split('-')[0]) >= 1955);
-	registerform_validation['email'] = regex_email.test($id("registerform-email").value);
-	registerform_validation['avatar'] = (avatar_ext == "jpg") || (avatar_ext == "jpeg");
+	window.registerform_validation['username'] = regex_username.test($id("registerform-username").value);
+	window.registerform_validation['password'] = regex_password.test($id("registerform-password").value);
+	window.registerform_validation['equalsUPE'] = ($id("registerform-password").value != $id("registerform-username").value) && ($id("registerform-password").value != $id("registerform-email").value);
+	window.registerform_validation['password2'] = ($id("registerform-password").value == $id("registerform-password2").value);
+	window.registerform_validation['fullname'] = regex_fullname.test($id("registerform-fullname").value);
+	window.registerform_validation['birthdate'] = (regex_birthdate.test($id("registerform-birthdate").value)) && (parseInt($id("registerform-birthdate").value.split('-')[0]) >= 1955);
+	window.registerform_validation['email'] = regex_email.test($id("registerform-email").value);
+	window.registerform_validation['avatar'] = (avatar_ext == "jpg") || (avatar_ext == "jpeg");
 
 	for(key in registerform_validation) {
 		if(!registerform_validation[key]) {
-			if(!hasClass(registerform[key], 'error')) {
-				var e = document.createElement('div');
-				addClass(e, 'errorMessage');
-				e.innerHTML = error_message[key];
 			
-				addClass(registerform[key], 'error');
-				registerform[key].parentNode.appendChild(e);
-			}
 			$id("registerform-submit").disabled = true;
 			ok = false;
-		} else if(hasClass(registerform[key], 'error')) {
-			console.log(registerform[key]);
-			deleteClass(registerform[key], 'error');
-			registerform[key].parentNode.removeChild(registerform[key].parentNode.lastChild);
-		}
+		} 
 	}
 	if(ok)
 		$id("registerform-submit").disabled = false;
 }
 
 function hasClass(x, y) {
+	console.log(x);
 	var classes = x.className.split(' ');
 	return (classes.indexOf(y) != -1);
 }
