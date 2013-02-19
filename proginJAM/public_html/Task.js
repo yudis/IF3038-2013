606f4865aedb1926f9eeef90d151c3839defe6b0
Jeremy Joseph Hanniel <evecrest@gmail.com>
2013-02-19 16:15:47 +0700

    New Task HTML & JS almost done.

--- proginJAM/public_html/Task.js
+++ proginJAM/public_html/Task.js
@@ -1,3 +1,9 @@
+if(!String.prototype.trim) {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g,'');
+    };
+}
+
 function EditTaskDetail() {
     var oldAssignee = document.getElementById('assignee_info').innerHTML;
     var editAssignee = "<input type='text'\n\
@@ -10,8 +16,9 @@ value=" + oldAssignee + "/>";
     var editCommentary = "<textarea id='commentary_edit'\n\
 rows='6'\n\
 cols='40'\n\
-form='task_detail_form'\n\
-value=" + oldCommentary + "></textarea>";
+form='task_detail_form'>\n\
+" + oldCommentary.trim() + "\n\
+</textarea>";
     document.getElementById('commentary').innerHTML = editCommentary;
     
     var oldTag = document.getElementById('tag_info').innerHTML;
@@ -20,6 +27,9 @@ id='tag_edit'\n\
 form='task_detail_form'\n\
 value=" + oldTag + "/>";
     document.getElementById('tag_info').innerHTML = editTag;
+    
+    var oldDeadline = document.getElementById('deadline').innerHTML;
+    // Change the deadline
     
     var doneButton = "<input type='submit'\n\
 id='done_edit_button'\n\
@@ -33,3 +43,20 @@ function DoneEditDetail() {
     
 }
 
+function ValidateFileType() {
+    var id_value = document.getElementById('attachment').value;
+    if (id_value !== '') {
+        var image_valid_extensions = /(.jpg|.jpeg|.gif|.png)$/i;
+        var video_valid_extensions = /(.avi|.flv|.mp4)$/i;
+        var file_valid_extensions = /(.txt|.doc|.pdf|.docx|.xls|.xlsx|.ppt|.pptx)$/i;
+        if (image_valid_extensions.test(id_value)) {
+            
+        } else if (video_valid_extensions.test(id_value)) {
+            
+        } else if (file_valid_extensions.test(id_value)) {
+            
+        } else {
+            
+        }
+    }
+}
