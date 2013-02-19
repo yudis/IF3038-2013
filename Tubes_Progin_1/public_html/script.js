0345b88e962ddcfa77bc27ddf8fbd138e94d9003
allirf <al_rischa17@yahoo.com>
2013-02-19 14:26:38 +0700

    +css, img

---
+++ Tubes_Progin_1/public_html/script.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showList(){
+document.getElementById("listtugas").style.visibility="visible";
+document.getElementById("rincitugas").style.visibility="hidden";
+document.getElementById("edittugas").style.visibility="hidden";
+document.getElementById("buattugas").style.visibility="hidden";
+self.focus();
+}
+
+function showRinci(){
+document.getElementById("listtugas").style.visibility="hidden";
+document.getElementById("rincitugas").style.visibility="visible";
+document.getElementById("edittugas").style.visibility="hidden";
+document.getElementById("buattugas").style.visibility="hidden";
+self.focus();
+}
+
+function showEdit(){
+document.getElementById("listtugas").style.visibility="hidden";
+document.getElementById("rincitugas").style.visibility="hidden";
+document.getElementById("edittugas").style.visibility="visible";
+document.getElementById("buattugas").style.visibility="hidden";
+}
+function showBuat(){
+document.getElementById("listtugas").style.visibility="hidden";
+document.getElementById("rincitugas").style.visibility="hidden";
+document.getElementById("edittugas").style.visibility="hidden";
+document.getElementById("buattugas").style.visibility="visible";
+}
