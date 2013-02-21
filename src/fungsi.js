905cd5383e84d05f3c3e947e61fa03007675e06d
apek-chen <arie.tando91@gmail.com>
2013-02-21 17:03:47 +0700

    ud ada header

--- src/fungsi.js
+++ src/fungsi.js
@@ -11,6 +11,18 @@ var valid5 = document.getElementById("valid5");
 var valid6 = document.getElementById("valid6");
 var inputfile = document.getElementById("inputfile");
 var submit = document.getElementById("submit");
+var submitlogin = document.getElementById("submitlogin");
+var idlogin = document.getElementById("idlogin");
+var passlogin = document.getElementById("passlogin");
+var form = document.getElementById("form");
+
+	form.onsubmit = function(){
+		if ((idlogin.value == "admin") && (passlogin.value == "admin"))
+		{
+			window.location = "dashboard.html";
+		}
+		return false;
+	}
 
 	username.onkeyup = function()
 	{
@@ -117,4 +129,37 @@ var submit = document.getElementById("submit");
 			valid6.src = "img/salah.png";
 			
 		cekvalid();
+	}
+	
+	function login()
+	{
+		if ((idlogin.value == "admin") && (passlogin.value == "admin"))
+		{
+			window.location = "dashboard.html";
+		}
+	}
+	
+	function buildCal(m, y, cM, cH, cDW, cD, brdr){
+	var mn=['January','February','March','April','May','June','July','August','September','October','November','December'];
+	var dim=[31,0,31,30,31,30,31,31,30,31,30,31];
+
+	var oD = new Date(y, m-1, 1); //DD replaced line to fix date bug when current day is 31st
+	oD.od=oD.getDay()+1; //DD replaced line to fix date bug when current day is 31st
+
+	var todaydate=new Date() //DD added
+	var scanfortoday=(y==todaydate.getFullYear() && m==todaydate.getMonth()+1)? todaydate.getDate() : 0 //DD added
+
+	dim[1]=(((oD.getFullYear()%100!=0)&&(oD.getFullYear()%4==0))||(oD.getFullYear()%400==0))?29:28;
+	var t='<div class="'+cM+'"><table class="'+cM+'" cols="7" cellpadding="0" border="'+brdr+'" cellspacing="0"><tr align="center">';
+	t+='<td colspan="7" align="center" class="'+cH+'">'+mn[m-1]+' - '+y+'</td></tr><tr align="center">';
+	for(s=0;s<7;s++)t+='<td class="'+cDW+'">'+"SMTWTFS".substr(s,1)+'</td>';
+	t+='</tr><tr align="center">';
+	for(i=1;i<=42;i++){
+	var x=((i-oD.od>=0)&&(i-oD.od<dim[m-1]))? i-oD.od+1 : '&nbsp;';
+	if (x==scanfortoday) //DD added
+	x='<span id="today">'+x+'</span>' //DD added
+	t+='<td class="'+cD+'">'+x+'</td>';
+	if(((i)%7==0)&&(i<36))t+='</tr><tr align="center">';
+	}
+	return t+='</tr></table></div>';
 	}
