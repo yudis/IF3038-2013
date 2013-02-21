b1a8addc7e2adbed5eaa2bbbe9fc3bfa935219e4
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-22 00:56:44 +0700

    move global function

--- js/global.js
+++ js/global.js
@@ -8,3 +8,30 @@
 		return ret;
 	}
 })(window);
+
+function hasClass(x, y) {
+	console.log(x);
+	var classes = x.className.split(' ');
+	return (classes.indexOf(y) != -1);
+}
+
+function addClass(x, y) {
+	var classes = x.className.split(' ');
+	if(classes.indexOf(y) != -1) {
+		return;
+	}
+	if(x.className === '') {
+		x.className += y;
+	} else {
+		x.className += ' ' + y;
+	}
+}
+
+function deleteClass(x, y) {
+	var classes = x.className.split(' ');
+	var index = classes.indexOf(y);
+	if(index != -1) {
+		classes.splice(index, 1);
+		x.className = classes.join(" ");
+	}
+}
