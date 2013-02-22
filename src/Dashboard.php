5aac8104ca4439b9b7725953d7529bd458a6f9e5
David Eko Wibowo <dvdekow@gmail.com>
2013-02-22 14:18:09 +0700

    Input data
    layout hampir fix

---
+++ src/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <script type="text/javascript" language="javascript" src="dashboard.js"></script>
+    <p id="welcome">your task</p>
+    <p id="cats"></p>
+    <button id="addTask" onclick="addTask()">add task</button>
+    <button id="addCategory" onclick="showCatPrompt()">add category</button>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>      
+    <title>Home Page</title>
+</head>
+<body>
+    <script type="text/javascript" language="javascript" src="registration.js"></script>
+    <form>
+        <p id="inputID">username: </p><input type="text" name="ID" id="ID" autocomplete="off">
+        <p id="inputPass">password: </p><input type="password" name="Password" id="userPassword" > <br>
+        <input type="submit" name="submitHome" value="login" onclick="closeSelf()"> <br>
+    </form>
+       <button id="S" onclick="appendRegistration(S)">sign up</button>
+       
+</body>   
+</html>
+
+
