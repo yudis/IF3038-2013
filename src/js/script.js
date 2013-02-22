2972a136d4d7a6f0071e06613df1be1fcef18ba8
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-22 20:41:53 +0700

    add everything

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,114 @@
 // JavaScript Document
-var TimeOut = 300;
-var currentLayer = null;
-var currentItem = null;
-var currentLayerNum = 0;
-var noClose = 0;
-var closeTimer = null;
 
-function mOpen(n) {
-	var l = document.getElementById("menu" + n);
-	var mm = document.getElementById("mmenu" + n);
+function keProfil() {
+	window.location = 'Profil.html';
+}
+
+function active_1() {
+	var elmt_1 = document.getElementById("dynamic_1");
+	elmt_1.style.visibility = "visible";
 	
-	if (l) {
-		//document.writeln("masuk");
-		mCancelCloseTime();
-		l.style.visibility = 'visible';
-		
-		if (currentLayer && (currentLayerNum != n)) {
-			currentLayer.style.visibility = 'hidden';
-		}
-		currentLayer = l;
-		currentItem = mm;
-	} else if (currentLayer) {
-		currentLayer.style.visibility = 'hidden';
-		currentLayerNum = 0;
-		currentItem = null;
-		currentLayer = null;
-	} //else document.writeln("ngga masuk mana mana");
+	var elmt_2 = document.getElementById("dynamic_2");
+	elmt_2.style.visibility = "hidden";
+	
+	var elmt_3 = document.getElementById("dynamic_3");
+	elmt_3.style.visibility = "hidden";
+	
+	var elmt_semuaTugas = document.getElementById("dynamic_semuaTugas");
+	elmt_semuaTugas.style.visibility = "hidden";
+	
+	var highlight_1 = document.getElementById("static_1");
+	highlight_1.style.background = "#F20";
+	
+	var highlight_2 = document.getElementById("static_2");
+	highlight_2.style.background = "#0F3";
+	
+	var highlight_3 = document.getElementById("static_3");
+	highlight_3.style.background = "#0F3";
+	
+	var highlight_semuaTugas = document.getElementById("semuaTugas");
+	highlight_semuaTugas.style.background = "#0F3";
 }
 
-function mCloseTime() {
-	closeTimer = window.setTimeout(mClose, TimeOut);
+function active_2() {
+	var elmt_1 = document.getElementById("dynamic_1");
+	elmt_1.style.visibility = "hidden";
+	
+	var elmt_2 = document.getElementById("dynamic_2");
+	elmt_2.style.visibility = "visible";
+	
+	var elmt_3 = document.getElementById("dynamic_3");
+	elmt_3.style.visibility = "hidden";
+	
+	var elmt_semuaTugas = document.getElementById("dynamic_semuaTugas");
+	elmt_semuaTugas.style.visibility = "hidden";
+	
+	var highlight_1 = document.getElementById("static_1");
+	highlight_1.style.background = "#0F3";
+	
+	var highlight_2 = document.getElementById("static_2");
+	highlight_2.style.background = "#F20";
+	
+	var highlight_3 = document.getElementById("static_3");
+	highlight_3.style.background = "#0F3";
+
+	var highlight_semuaTugas = document.getElementById("semuaTugas");
+	highlight_semuaTugas.style.background = "#0F3";
 }
 
-function mCancelCloseTime() {
-	//document.writeln("masuk mCancelCloseTime");
-	if (closeTimer) {
-		window.clearTimeout(closeTimer);
-		closeTimer = null;
-	}
-	//document.writeln("keluar mCancelCloseTime");
+function active_3() {
+	var elmt_1 = document.getElementById("dynamic_1");
+	elmt_1.style.visibility = "hidden";
+	
+	var elmt_2 = document.getElementById("dynamic_2");
+	elmt_2.style.visibility = "hidden";
+	
+	var elmt_3 = document.getElementById("dynamic_3");
+	elmt_3.style.visibility = "visible";
+	
+	var elmt_semuaTugas = document.getElementById("dynamic_semuaTugas");
+	elmt_semuaTugas.style.visibility = "hidden";
+	
+	var highlight_1 = document.getElementById("static_1");
+	highlight_1.style.background = "#0F3";
+	
+	var highlight_2 = document.getElementById("static_2");
+	highlight_2.style.background = "#0F3";
+	
+	var highlight_3 = document.getElementById("static_3");
+	highlight_3.style.background = "#F20";
+	
+	var highlight_semuaTugas = document.getElementById("semuaTugas");
+	highlight_semuaTugas.style.background = "#0F3";
 }
 
-function mClose() {
-	if (currentLayer && noClose != 1) {
-		currentLayer.style.visibility = 'hidden';
-		currentLayerNum = 0;
-		currentLayer = null;
-		currentItem = null;
-	} else noClose = 0;
-	currentLayer = null;
-	currentItem = null;
+function active_semuaTugas(){
+	var elmt_1 = document.getElementById("dynamic_1");
+	elmt_1.style.visibility = "hidden";
+	
+	var elmt_2 = document.getElementById("dynamic_2");
+	elmt_2.style.visibility = "hidden";
+	
+	var elmt_3 = document.getElementById("dynamic_3");
+	elmt_3.style.visibility = "hidden";
+	
+	var elmt_semuaTugas = document.getElementById("dynamic_semuaTugas");
+	elmt_semuaTugas.style.visibility = "visible";
+	
+	var highlight_1 = document.getElementById("static_1");
+	highlight_1.style.background = "#0F3";
+	
+	var highlight_2 = document.getElementById("static_2");
+	highlight_2.style.background = "#0F3";
+	
+	var highlight_3 = document.getElementById("static_3");
+	highlight_3.style.background = "#0F3";
+	
+	var highlight_semuaTugas = document.getElementById("semuaTugas");
+	highlight_semuaTugas.style.background = "#F20";
 }
 
-document.onclick = mClose();
-
+function showPopUp() {
+	var popUpArea = document.getElementById("popUp");
+	popUpArea.style.visibility = "visible";
+}
