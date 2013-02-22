e403e1054fda8dfbe7771a4f918611386387f25a
apek-chen <arie.tando91@gmail.com>
2013-02-22 21:07:04 +0700

    bikin slideshow

--- src/fungsi.js
+++ src/fungsi.js
@@ -136,3 +136,33 @@ var form = document.getElementById("form");
 			
 		cekvalid();
 	}
+
+var crossFadeDuration = 3;
+var Pic = new Array();
+Pic[0] = 'img/ilustrate1.JPG'
+Pic[1] = 'img/ilustrate2.JPG'
+Pic[2] = 'img/ilustrate3.JPG'
+Pic[3] = 'img/ilustrate4.JPG'
+
+var t;
+var j = 0;
+var p = Pic.length;
+var preLoad = new Array();
+for (i = 0; i < p; i++) {
+preLoad[i] = new Image();
+preLoad[i].src = Pic[i];
+}
+function SlideShow() {
+	if (document.all) {
+	document.images.SlideShow.style.filter="blendTrans(duration=2)";
+	document.images.SlideShow.style.filter="blendTrans(duration=crossFadeDuration)";
+	document.images.SlideShow.filters.blendTrans.Apply();
+	}
+	document.images.SlideShow.src = preLoad[j].src;
+	if (document.all) {
+	document.images.SlideShow.filters.blendTrans.Play();
+	}
+	j = j + 1;
+	if (j > (p - 1)) j = 0;
+	t = setTimeout('SlideShow()', 2000);
+}
