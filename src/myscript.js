d684314918e8d0d6f80cd934abaf8fc3a32535bf
atikayusuf <yusuf.atika@gmail.com>
2013-02-22 21:49:44 +0700

    commit

--- src/myscript.js
+++ src/myscript.js
@@ -6,6 +6,18 @@ function myfunc2(categ){
 	return categ.assignee;
 }
 
+function myfunc3(categ){
+	return categ.deadline;
+}
+
+function myfunc4(categ){
+	return categ.comment;
+}
+
+function myfunc5(categ){
+	return categ.tag;
+}
+
 
 function addtask(task, deadline, assignee, comment, tag){
 	this.task = task;
@@ -21,19 +33,22 @@ function addcateg(newcateg){
 }
 
 function createcateg(){
+	var d1 = new Date("October 13, 2013 10:13:00")
+	var d2 = new Date("July 12, 2014 07:13:00")
+	var d3 = new Date("September 29, 1955 11:13:00")
 	//categ={cat:cat, task:task, deadline:deadline, assignee:assignee, comment:comment, tag:tag};//var task1 = new Array("Buy a Ferrari","Rent an apartment","Build a startup","Fail in life");
-	categ1={task:["Buy a Ferrari","Rent an apartment","Build a startup","Buy some milk"], deadline:(2013,02,22), assignee:"Atika", comment:"it works", tag:"#work"};
+	categ1={task:["Buy a Ferrari","Rent an apartment","Build a startup","Buy some milk"], deadline:d1, assignee:"Cow", comment:"hurry", tag:"#misc #food"};
 	
 	//var task2 = new Array("Buy a Lexus","Rent an apartment","Build a startup","Fail in life");
-	categ2={task:["Buy a Lexus","Sleep beside a hobo","Build a startup","Fail in life"], deadline:(2013,02,22), assignee:"Atika", comment:"it works", tag:"#work"};
+	categ2={task:["Buy a Lexus","Sleep beside a hobo","Fail in life","Build a startup"], deadline:d2, assignee:"Atika", comment:"start small", tag:"#work #money"};
 	
 	//var task3 = new Array("Buy a Volkswagen","Rent an apartment","Build a startup","Fail in life");
-	categ3={task:["Buy a Volkswagen","Buy a house","Build a startup","Make a sandwich"], deadline:(2013,02,22), assignee:"Atika", comment:"it works", tag:"#work"};
+	categ3={task:["Buy a Volkswagen","Buy a house","Build a startup","Go back to the future"], deadline:d3, assignee:"Marty", comment:"urgent matters", tag:"#project #time"};
 }
 
 
 
-function mOver(categ)
+function mOver1(categ)
 {
 	
 	createcateg();
@@ -41,8 +56,38 @@ function mOver(categ)
 	for (var i in categ.task)
 	{
 		
-		document.getElementById("tasklist").innerHTML=myfunc(categ,i);
+		document.getElementById("tasklist").innerHTML='<b><a href="task.html">'+myfunc(categ,i)+'<br>'+myfunc3(categ).toUTCString()+'</br>'+'<br>'+'<br>'+myfunc4(categ)+'</br>'+'<br>'+myfunc2(categ)+'</br>'+'<br>'+myfunc5(categ)+'</br>'+'</a></b><br><a href="addtask.html">&laquo; Add more tasks..</a></br>';
 			
+	}
+	
+		
+	
+}
+
+function mOver2(categ)
+{
+	
+	createcateg();
+	
+	for (var i in categ.task)
+	{
+		
+		document.getElementById("tasklist").innerHTML='<b><a href="task.html">'+myfunc(categ,i)+'<br>'+myfunc3(categ).toUTCString()+'</br>'+'<br>'+'<br>'+myfunc4(categ)+'</br>'+'<br>'+myfunc2(categ)+'</br>'+'<br>'+myfunc5(categ)+'</br>'+'</a></b><br><a href="addtask.html">&laquo; Add more tasks..</a></br>';
+					
+	}
+	
+}
+
+function mOver3(categ)
+{
+	
+	createcateg();
+	
+	for (var i in categ.task)
+	{
+		
+		document.getElementById("tasklist").innerHTML='<b><a href="task.html">'+myfunc(categ,i)+'<br>'+myfunc3(categ).toUTCString()+'</br>'+'<br>'+'<br>'+myfunc4(categ)+'</br>'+'<br>'+myfunc2(categ)+'</br>'+'<br>'+myfunc5(categ)+'</br>'+'</a></b><br><a href="addtask.html">&laquo; Add more tasks..</a></br>';
+					
 	}
 	
 }
@@ -50,10 +95,4 @@ function mOver(categ)
 function mOut(obj)
 {
 	//document.getElementById("tasklist").innerHTML=mclk(tasklist);
-	
-	
-}
-
-function mclk(){
-	//document.getElementById("tasklist").onclick=function(){document.getElementById("tasklist").innerHTML="<a href="newtask.html"></a>"};
 }
