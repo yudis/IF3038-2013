0d02319d290b41fe36585cf53d77f68dfb7d3b3b
allirf <al_rischa17@yahoo.com>
2013-02-22 21:55:12 +0700

    commit terakhir

--- Tubes_Progin_1/public_html/script.js
+++ Tubes_Progin_1/public_html/script.js
@@ -71,36 +71,14 @@ var valid = false;
 
 function createTask() {
     var regex = /^[a-zA-Z0-9]{5,25}$/;
-    var fuData = document.getElementById('upload');
-        var FileUploadPath = fuData.value;
-
-        if (FileUploadPath == '') {
-            // There is no file selected 
-            valid = false;
-        }
-        else {
-            var Extension = FileUploadPath.substring(FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
-            if (Extension == "gif" || Extension == "png" || Extension == "bmp" || Extension == "jpeg") {
-                valid = true; // Valid file type
-                FileUploadPath == '';
-            }
-            else {
-                valid = false; // Not valid file type
-            }
-        }
-
     
-    if ((regex.test(document.getElementById("namaTask").value))&&(valid)){
+    if ((regex.test(document.getElementById("namaTask").value))){
         var k = document.getElementById("listtugas");
         k.innerHTML = "<a class='listTugas' onclick='showRinci();'><a class='listTugas' onclick='showRinci();'></a> <a class='listTugas' onclick='showRinci();'></a><a class='listTugas' onclick='showRinci();'></a><a class='listTugas' onclick='showRinci();'></a><a class='listTugas' onclick='showRinci();'></a><a class='listTugas' onclick='showRinci();'></a><a class='listTugas' onclick='showRinci();'></a><a onclick='showBuat()' class='addTask'></a>";
         document.getElementById("namaTask").value = 0;
         showList();
     } else {
-        if((regex.test(document.getElementById("namaTask").value))&&!(valid)) {
-            alert("please choose the correct attachment");
-        } else {
-            alert("Task's name must be between 5-25");
-        }
+            alert("task name must be 5-25 long");
     }
 }
 
@@ -130,4 +108,68 @@ function restore() {
    document.body.removeChild(document.getElementById("overlay"));
    document.getElementById('add').style.display='none';
    document.getElementById('overlay').style.display='none';
+}
+
+var clickable = false;
+
+function Redirect(){
+    window.location = "index.html";
+}
+
+function Login(){
+    if (document.getElementById("logusername").value !== "admin"){
+        alert("Wrong username!");
+    } else if (document.getElementById("logpassword").value !== "admincool"){
+        alert("Wrong password!");
+    }else{
+        window.location = "Dashboard.html";
+        localStorage.username = document.getElementById("logusername").value;
+        localStorage.name = "Billy The Kid";
+        localStorage.date = "1968-09-3";
+        localStorage.email = "coolKid@yahoo.com";
+        document.getElementById("foto").src = "img/foto.png";
+    }
+}
+
+function Register(){
+    var atPos = document.getElementById("regemail").value.indexOf("@");
+    var dotPos = document.getElementById("regemail").value.indexOf(".");
+    if ((document.getElementById("regusername").value.length < 5) && (document.getElementById("regusername").value !== "")){
+        alert("Username should be at least 5 characters long.");
+    } else if ((document.getElementById("regpassword1").value.length < 8) && (document.getElementById("regpassword1").value !== "")){
+        alert("Password should be at least 8 characters long.");
+    } else if ((document.getElementById("regusername").value === document.getElementById("regpassword1").value) && (document.getElementById("regusername").value !== "") && (document.getElementById("regpassword1").value !== "")){
+        alert("Username and password cannot be identical.");
+    } else if ((document.getElementById("regpassword1").value !== document.getElementById("regpassword2").value) && (document.getElementById("regpassword2").value !== "") && (document.getElementById("regpassword2").value !== "")){
+        alert("Confirmed password and password are not the same.");
+    } else if ((document.getElementById("regname").value.indexOf(" ") < 0) && (document.getElementById("regname").value !== "")) {
+        alert("Name should be constructed by two or more words separated by space.");
+    } else if ((document.getElementById("regemail").value !== "") && (atPos < 1)){
+            alert("There should be at least one character before '@' character.");
+    } else if ((document.getElementById("regemail").value !== "") && (dotPos - atPos < 2)){
+            alert("There should be at least one character between '@' and '.' character.");
+    } else if ((document.getElementById("regemail").value !== "")&&(document.getElementById("regemail").value.length - dotPos < 3)){
+            alert("There should be at least two characters after '.' character");
+    } else if ((document.getElementById("regusername").value !== "")&&(document.getElementById("regpassword1").value !== "")&&
+        (document.getElementById("regpassword2").value !== "")&&(document.getElementById("regname").value !== "")&&
+        (document.getElementById("regemail").value !== "")){
+            document.getElementById("regbutton").style.color = "black";
+            document.getElementById("regbutton").style.fontWeight = "bold";
+            clickable = true;
+    } else {
+        document.getElementById("regbutton").style.color = "#777777";
+        document.getElementById("regbutton").style.fontWeight = "normal";
+        clickable = false;
+    }
+}
+
+function Submit(){
+    if (clickable){
+        window.location = "Dashboard.html";
+        localStorage.username = document.getElementById("regusername").value;
+        localStorage.name = document.getElementById("regname").value;
+        localStorage.date = document.getElementById("regdate").value;
+        localStorage.email = document.getElementById("regemail").value;
+        document.getElementById("foto").src = "img/foto_anonim.png";
+    }
 }
