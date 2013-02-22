c50bdc3c081d2993b862b917d335a294bd09ca9f
Ruth Nattassha <salvaterarug@gmail.com>
2013-02-22 21:17:16 +0700

    FinishTask()

--- eurilys/dekstop/js/animation.js
+++ eurilys/dekstop/js/animation.js
@@ -129,8 +129,35 @@ function signup() {
 }
 
 function finishTask(i) {
-	document.getElementById("curtask"+i).style.display = "none";
-	document.getElementById("curtask"+i+1).style.top = "200px";
+	if (i == 1) {
+		document.getElementById("curtask1").style.opacity = "0";
+		document.getElementById("curtask2").style.top = "20px";
+		document.getElementById("curtask3").style.top = "200px";
+		document.getElementById("curtask4").style.top = "380px";
+		document.getElementById("curtask5").style.top = "560px";
+		}
+		
+		else {if (i == 2) {
+			document.getElementById("curtask2").style.opacity = "0";
+			document.getElementById("curtask3").style.top = "200px";
+			document.getElementById("curtask4").style.top = "380px";
+			document.getElementById("curtask5").style.top = "560px";
+		}
+		else {
+			if (i==3) {
+				document.getElementById("curtask3").style.opacity = "0";
+				document.getElementById("curtask4").style.top = "380px";
+				document.getElementById("curtask5").style.top = "560px";
+			}
+			else if (i == 4) {
+				document.getElementById("curtask4").style.opacity = "0";
+				document.getElementById("curtask5").style.top = "560px";
+			}
+			else if (i == 5){
+				document.getElementById("curtask5").style.opacity = "0";
+			}
+		}
+	}
 }
 
 function checkTaskName() {
