717b377b12bcaed4f527c5ff412423cc39ad9f21
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-22 13:37:22 +0700

    added real login function but still cant redirecting pages

--- js/login.js
+++ js/login.js
@@ -44,6 +44,52 @@ window.onload = function() {
 		}
 	}
 	
+	//init_date();
+	
+}
+
+function login_check() {
+	var a = $id("loginform-username").value.length != 0;
+	var b = $id("loginform-password").value.length != 0;
+	if(a && b) {
+		$id("loginform-submit").disabled = false;
+	} else {
+		$id("loginform-submit").disabled = true;
+	}
+}
+
+function register_check() {
+	
+	var regex_username = /^[A-Za-z0-9_]{5,}$/;
+	var regex_password = /^[A-Za-z0-9!@()$%^&*#_]{8,}$/;
+	var regex_email = /^[A-Za-z][A-Za-z0-9_.]*\@[A-Za-z0-9_.-]+\.[A-Za-z][A-Za-z]+$/;
+	var regex_fullname = /^[A-Za-z]+ [A-Za-z \.]+$/;
+	var regex_birthdate = /^\d{4}\-\d{2}\-\d{2}$/;
+	var avatar_ext = $id("registerform-avatar").value.split('.').pop();
+	
+	var ok = true;
+
+	window.registerform_validation['username'] = regex_username.test($id("registerform-username").value);
+	window.registerform_validation['password'] = regex_password.test($id("registerform-password").value);
+	window.registerform_validation['equalsUPE'] = ($id("registerform-password").value != $id("registerform-username").value) && ($id("registerform-password").value != $id("registerform-email").value);
+	window.registerform_validation['password2'] = ($id("registerform-password").value == $id("registerform-password2").value);
+	window.registerform_validation['fullname'] = regex_fullname.test($id("registerform-fullname").value);
+	window.registerform_validation['birthdate'] = (regex_birthdate.test($id("registerform-birthdate").value)) && (parseInt($id("registerform-birthdate").value.split('-')[0]) >= 1955);
+	window.registerform_validation['email'] = regex_email.test($id("registerform-email").value);
+	window.registerform_validation['avatar'] = (avatar_ext == "jpg") || (avatar_ext == "jpeg");
+
+	for(key in registerform_validation) {
+		if(!registerform_validation[key]) {
+			
+			$id("registerform-submit").disabled = true;
+			ok = false;
+		} 
+	}
+	if(ok)
+		$id("registerform-submit").disabled = false;
+}
+
+function init_date() {
 	// init date
 	var year = $id('registerform-birthdate-year');
 	var month = $id('registerform-birthdate-month');
@@ -90,44 +136,15 @@ window.onload = function() {
 	}
 }
 
-function login_check() {
-	var a = $id("loginform-username").value.length != 0;
-	var b = $id("loginform-password").value.length != 0;
-	if(a && b) {
-		$id("loginform-submit").disabled = false;
-	} else {
-		$id("loginform-submit").disabled = true;
+function try_login() {
+	if (!Session.login(
+		$id("loginform-username").value,
+		$id("loginform-password").value
+	)) {
+		alert("Incorrect username or password");
+	} else{
+		alert("Login success");
+		window.location("Profil.html");
 	}
-}
-
-function register_check() {
-	
-	var regex_username = /^[A-Za-z0-9_]{5,}$/;
-	var regex_password = /^[A-Za-z0-9!@()$%^&*#_]{8,}$/;
-	var regex_email = /^[A-Za-z][A-Za-z0-9_.]*\@[A-Za-z0-9_.-]+\.[A-Za-z][A-Za-z]+$/;
-	var regex_fullname = /^[A-Za-z]+ [A-Za-z \.]+$/;
-	var regex_birthdate = /^\d{4}\-\d{2}\-\d{2}$/;
-	var avatar_ext = $id("registerform-avatar").value.split('.').pop();
-	
-	var ok = true;
-
-	window.registerform_validation['username'] = regex_username.test($id("registerform-username").value);
-	window.registerform_validation['password'] = regex_password.test($id("registerform-password").value);
-	window.registerform_validation['equalsUPE'] = ($id("registerform-password").value != $id("registerform-username").value) && ($id("registerform-password").value != $id("registerform-email").value);
-	window.registerform_validation['password2'] = ($id("registerform-password").value == $id("registerform-password2").value);
-	window.registerform_validation['fullname'] = regex_fullname.test($id("registerform-fullname").value);
-	window.registerform_validation['birthdate'] = (regex_birthdate.test($id("registerform-birthdate").value)) && (parseInt($id("registerform-birthdate").value.split('-')[0]) >= 1955);
-	window.registerform_validation['email'] = regex_email.test($id("registerform-email").value);
-	window.registerform_validation['avatar'] = (avatar_ext == "jpg") || (avatar_ext == "jpeg");
-
-	for(key in registerform_validation) {
-		if(!registerform_validation[key]) {
-			
-			$id("registerform-submit").disabled = true;
-			ok = false;
-		} 
-	}
-	if(ok)
-		$id("registerform-submit").disabled = false;
 }
 
--- js/session.js
+++ js/session.js
@@ -1,3 +1,103 @@
-/**
+/**bj
  * @author gmochid
  */
+
+(function($) {
+	$.User = function(username, password, email, fullname, birthdate, avatar) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.fullname = fullname;
+		this.birthdate = birthdate;
+		this.avatar = avatar;
+	}
+	
+	$.Users = {
+		serialize: function (users) {
+			return JSON.stringify(users);
+		},
+		
+		deserialize: function (jsonstring) {
+			if ((jsonstring === '') && (jsonstring === undefined)) {
+				return Array();
+			}
+			
+			var users = [];
+			var objs = JSON.parse(jsonstring);
+			
+			for (var i=0; i < objs.length; i++) {
+				users.push( new User (
+					objs[i].username, 
+					objs[i].password, 
+					objs[i].email,       
+					objs[i].fullname, 
+					objs[i].birthdate,
+					objs[i].avatar     
+				))
+			}
+			
+			return users;
+		},
+		
+		load: function () {
+			return $.Users.deserialize(window.localStorage['Users']);
+		},
+		
+		save: function(users) {
+			window.localStorage['Users'] = $.Users.serialize(users);
+		},
+		
+		clear: function() {
+			window.localStorage.removeItem('Users');
+			return Array();
+		},
+		
+		add: function(user) {
+			var users = this.load();
+			users.push(user);
+			this.save(users);
+		}
+	}
+	
+	$.Session = {
+		login: function(username, password) {
+			var users = $.Users.load();
+			
+			var user = undefined;
+			for (var i=0; i < users.length; i++) {
+				if (users[i].username === username) {
+					user = users[i];
+					break;
+				}
+			}
+			
+			if (user == undefined) {
+				console.log("ERROR: username not found");
+				return false;
+			}
+			
+			if (password == user.password) {
+				window.localStorage['login'] = 'true';
+				window.localStorage['login_user'] = JSON.stringify(user);
+				
+				console.log("Login Success for " + user.username);
+				return true;
+			}
+			
+			console.log("ERROR: incorrect password");
+			return false;
+		}
+	}
+	
+	if ($.Users.load().length == 0) {
+		console.log(0);
+		$.Users.add(new User(
+			"sonny",
+			"fabrianoktavino",
+			"unyu@aneh.com",
+			"Agatha Christie",
+			"1990-02-03",
+			"tes.jpg"
+		));
+	};
+})(window)
