166450c076091373b2f5931cb56af8b656497490
Stefanus Thobi Sinaga <dibunuhgakmati@yahoo.com>
2013-02-22 10:55:20 +0700

    index.js + index.css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,29 @@
+var num=1
+img1 = new Image ()
+img1.src = "images/image-1.jpg"
+img2 = new Image ()
+img2.src = "images/image-2.jpg"
+img3 = new Image ()
+img3.src = "images/image-3.jpg"
+
+function next()
+{
+	num = num + 1
+	if (num == 4) {
+		num = 1
+	}
+	document.slider_picture.src=eval("img"+num+".src")
+}
+
+function back()
+{
+	num = num - 1
+	if (num == 0) {
+		num = 3
+	}
+	document.slider_picture.src=eval("img"+num+".src")
+}
+
 function validateForm()
 {
 	var xUsername=document.forms["registration"]["uname"].value;
@@ -6,31 +32,31 @@ function validateForm()
 		alert("User name must be filled out with at least 5 characters.");
 		return false;
 	}
-        var xPassword=document.forms["registration"]["pwd"].value;
+    var xPassword=document.forms["registration"]["pwd"].value;
 	if ((xPassword.length < 8) || (xPassword == xUsername))
 	{
 		alert("Password must be filled out with at least 8 characters and must be different with username.");
 		return false;
 	}
-        var xPasswordConfirm=document.forms["registration"]["pwd_confirm"].value;
+    var xPasswordConfirm=document.forms["registration"]["pwd_confirm"].value;
 	if (xPasswordConfirm != xPassword)
 	{
 		alert("Your password confirmation doesn't match.");
 		return false;
 	}
-        var xName=document.forms["registration"]["name"].value;
+    var xName=document.forms["registration"]["name"].value;
 	var flag = 0;
-        var i=0;
-        while (i<xName.length && (flag < 3)) { 
-            if ((xName.toString().charAt(i) != ' ') && (flag == 0))
-                flag = 1;
-			else if ((xName.toString().charAt(i) == ' ') && (flag == 1))
-				flag = 2;
-			else if ((xName.toString().charAt(i) != ' ') && (flag == 2))
-				flag = 3;
-            i++;
-        }
-        if ((flag < 3) || (xName.length < 3))
+    var i=0;
+    while (i<xName.length && (flag < 3)) { 
+		if ((xName.toString().charAt(i) != ' ') && (flag == 0))
+			flag = 1;
+		else if ((xName.toString().charAt(i) == ' ') && (flag == 1))
+			flag = 2;
+		else if ((xName.toString().charAt(i) != ' ') && (flag == 2))
+			flag = 3;
+		i++;
+    }
+    if ((flag < 3) || (xName.length < 3))
 	{
             alert("Name must contain at least two words.");
             return false;
