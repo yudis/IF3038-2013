7774dd263241eff87260a4acf8d9e5c7a2811e32
janicelaksana <janzz_bluefavor@hotmail.com>
2013-03-11 20:37:27 +0700

    yang udah diinsert value

--- Janice/basisdata/progin_405_13510035.sql
+++ Janice/basisdata/progin_405_13510035.sql
@@ -1,85 +1,104 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.8, for Win32 (x86)
 --
--- Host: localhost
--- Generation Time: Mar 11, 2013 at 12:40 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: progin_405_13510035
+-- ------------------------------------------------------
+-- Server version	5.5.8
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `progin_405_13510035`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attachment`
 --
 
-CREATE TABLE IF NOT EXISTS `attachment` (
+DROP TABLE IF EXISTS `attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attachment` (
   `attachment_id` varchar(255) NOT NULL,
   `task_id` varchar(255) NOT NULL,
   `link` varchar(255) NOT NULL,
   PRIMARY KEY (`attachment_id`),
-  KEY `task_id` (`task_id`)
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `attachment_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `attachment`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `attachment` WRITE;
+/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
   `category_id` varchar(255) NOT NULL,
-  `category_name` int(255) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
   PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `category`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('C-001','Home'),('C-002','Office');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `category_incharge`
 --
 
-CREATE TABLE IF NOT EXISTS `category_incharge` (
+DROP TABLE IF EXISTS `category_incharge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category_incharge` (
   `category_id` varchar(255) NOT NULL,
   `people_incharge` varchar(255) NOT NULL,
   KEY `category_id` (`category_id`),
-  KEY `people_incharge` (`people_incharge`)
+  KEY `people_incharge` (`people_incharge`),
+  CONSTRAINT `category_incharge_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE,
+  CONSTRAINT `category_incharge_ibfk_2` FOREIGN KEY (`people_incharge`) REFERENCES `user` (`username`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `category_incharge`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `category_incharge` WRITE;
+/*!40000 ALTER TABLE `category_incharge` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category_incharge` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `comment`
 --
 
-CREATE TABLE IF NOT EXISTS `comment` (
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
   `comment_id` varchar(255) NOT NULL,
   `commented_task` varchar(255) NOT NULL,
   `writer` varchar(255) NOT NULL,
@@ -87,97 +106,133 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `comment` text NOT NULL,
   PRIMARY KEY (`comment_id`),
   KEY `commented_task` (`commented_task`),
-  KEY `writer` (`writer`)
+  KEY `writer` (`writer`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`commented_task`) REFERENCES `task` (`task_id`) ON DELETE CASCADE,
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`writer`) REFERENCES `user` (`username`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `comment`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tag`
 --
 
-CREATE TABLE IF NOT EXISTS `tag` (
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
   `tag_id` varchar(255) NOT NULL,
   `tag_name` varchar(255) NOT NULL,
   PRIMARY KEY (`tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tag`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `task`
 --
 
-CREATE TABLE IF NOT EXISTS `task` (
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
   `task_id` varchar(255) NOT NULL,
   `task_name` varchar(255) NOT NULL,
   `status` tinyint(1) NOT NULL,
   `deadline` date NOT NULL,
-  `assignee` varchar(255) NOT NULL,
   `task_category` varchar(255) NOT NULL,
   PRIMARY KEY (`task_id`),
-  KEY `assignee` (`assignee`),
-  KEY `task_category` (`task_category`)
+  KEY `task_category` (`task_category`),
+  CONSTRAINT `task_ibfk_4` FOREIGN KEY (`task_category`) REFERENCES `category` (`category_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `task`
 --
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasktag`
---
-
-CREATE TABLE IF NOT EXISTS `tasktag` (
-  `task_id` varchar(255) NOT NULL,
-  `tag_id` varchar(255) NOT NULL,
-  KEY `task_id` (`task_id`),
-  KEY `tag_id` (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tasktag`
---
-
-
--- --------------------------------------------------------
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES ('T-001','Tugas Progin',1,'2013-03-23','C-002'),('T-002','Tugas Kripto',0,'2013-03-23','C-002');
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `task_incharge`
 --
 
-CREATE TABLE IF NOT EXISTS `task_incharge` (
+DROP TABLE IF EXISTS `task_incharge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_incharge` (
   `task_id` varchar(255) NOT NULL,
   `people_incharge_task` varchar(255) NOT NULL,
   KEY `task_id` (`task_id`),
-  KEY `people_incharge_task` (`people_incharge_task`)
+  KEY `people_incharge_task` (`people_incharge_task`),
+  CONSTRAINT `task_incharge_ibfk_2` FOREIGN KEY (`people_incharge_task`) REFERENCES `user` (`username`) ON DELETE CASCADE,
+  CONSTRAINT `task_incharge_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `task_incharge`
 --
 
+LOCK TABLES `task_incharge` WRITE;
+/*!40000 ALTER TABLE `task_incharge` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_incharge` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `tasktag`
+--
+
+DROP TABLE IF EXISTS `tasktag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasktag` (
+  `task_id` varchar(255) NOT NULL,
+  `tag_id` varchar(255) NOT NULL,
+  KEY `task_id` (`task_id`),
+  KEY `tag_id` (`tag_id`),
+  CONSTRAINT `tasktag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`) ON DELETE CASCADE,
+  CONSTRAINT `tasktag_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasktag`
+--
+
+LOCK TABLES `tasktag` WRITE;
+/*!40000 ALTER TABLE `tasktag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasktag` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `fullname` varchar(255) NOT NULL,
@@ -185,53 +240,25 @@ CREATE TABLE IF NOT EXISTS `user` (
   `birthday` date NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('Adriel','2703','Nikodemus Adriel','13510089@std.stei.itb.ac.id','1993-03-27'),('Janice','1301','Janice Laksana','13510035@std.stei.itb.ac.id','1992-01-13'),('Thobi','1809','Stefanus Thobi','13510029@std.stei.itb.ac.id','1992-09-18');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for dumped tables
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Constraints for table `attachment`
---
-ALTER TABLE `attachment`
-  ADD CONSTRAINT `attachment_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `category_incharge`
---
-ALTER TABLE `category_incharge`
-  ADD CONSTRAINT `category_incharge_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `category_incharge_ibfk_2` FOREIGN KEY (`people_incharge`) REFERENCES `user` (`username`) ON DELETE CASCADE;
-
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`commented_task`) REFERENCES `task` (`task_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`writer`) REFERENCES `user` (`username`) ON DELETE CASCADE;
-
---
--- Constraints for table `task`
---
-ALTER TABLE `task`
-  ADD CONSTRAINT `task_ibfk_3` FOREIGN KEY (`assignee`) REFERENCES `user` (`username`) ON DELETE CASCADE,
-  ADD CONSTRAINT `task_ibfk_4` FOREIGN KEY (`task_category`) REFERENCES `category` (`category_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `tasktag`
---
-ALTER TABLE `tasktag`
-  ADD CONSTRAINT `tasktag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `tasktag_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `task_incharge`
---
-ALTER TABLE `task_incharge`
-  ADD CONSTRAINT `task_incharge_ibfk_2` FOREIGN KEY (`people_incharge_task`) REFERENCES `user` (`username`) ON DELETE CASCADE,
-  ADD CONSTRAINT `task_incharge_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE;
+-- Dump completed on 2013-03-11 20:34:31
