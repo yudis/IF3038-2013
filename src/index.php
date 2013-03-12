0574c8b1b9743c9cf4a75e8ad700bd267f475c94
meckyr <ecky_dozha@yahoo.co.id>
2013-03-11 22:50:02 -0700

    tambah calender multi browser

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@
 		<title>Shared to-do List</title>
 		<link rel="stylesheet" type="text/css" href="page/style.css">
 		<script type="text/javascript" src="page/index.js"></script>
+		<script src="page/calendar.js"></script>
+		<link href="page/calendar.css" rel="stylesheet">
 	</head>
 	<body onLoad="hide_submit_button();show_register_form();">
 		<div id="index-body">
@@ -48,62 +50,7 @@
 				<!--Full name (min. 2 spaces between 2 characters)-->
 				<input type="text" id="fullname" name="textFullName" onKeyUp="check_full_name()"/><br />
 				<!--Birth date (drop down, year >= 1955)-->
-				<select id="day">
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-					<option value="11">11</option>
-					<option value="12">12</option>
-					<option value="13">13</option>
-					<option value="14">14</option>
-					<option value="15">15</option>
-					<option value="16">16</option>
-					<option value="17">17</option>
-					<option value="18">18</option>
-					<option value="19">19</option>
-					<option value="20">20</option>
-					<option value="21">21</option>
-					<option value="22">22</option>
-					<option value="23">23</option>
-					<option value="24">24</option>
-					<option value="25">25</option>
-					<option value="26">26</option>
-					<option value="27">27</option>
-					<option value="28">28</option>
-					<option value="29">29</option>
-					<option value="30">30</option>
-					<option value="31">31</option>
-				</select>&nbsp;
-				<select id="month">
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-					<option value="11">11</option>
-					<option value="12">12</option>
-				</select>&nbsp;
-				
-				<?php 
-					$i = 0;
-					echo "<select id=\"year\">";
-					for($i = 1955 ; $i < 2013 ; $i++){
-						echo "<option value=\"$i\">$i</option>";
-					}
-					echo "</select>";
-				?>
+				<input type="text" class="calendarSelectDate" /><div id="calendarDiv"></div>
 				<br />
 				<!--email, validation:
 					min. 1 character before @
