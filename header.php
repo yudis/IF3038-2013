eb35519c8be19733a37f844a72a6727890a227fa
meckyr <ecky_dozha@yahoo.co.id>
2013-03-11 19:55:15 -0700

    header ditambahkan

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Header</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="header-logo"><a href="dashboard.php"><img src="logo.png" width="100px" height="60px"/></a></div>
+		<div id="header-title"><a href="dashboard.php"><img src="title.png" width="250px" height="80px"/></a></div>
+		<div id="header-link"><a href="dashboard.php"><b>Go To Dashboard</b></a></div>
+		<div id="header-right-side">
+			<div><input type="text" placeholder="Search Tasks"><button>Search</button></div>
+			<div id="header-right-user">
+				You logged as, <b>meckyr</b>
+				<ul>
+					<li><a href="profile.php">Go to Profile</a></li>
+					<li><a href="index.php">Sign Out</a></li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
