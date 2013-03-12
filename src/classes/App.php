df745989f47b8da576afe0b316b02de1aeb83774
Abraham Krisnanda S <13510033@std.stei.itb.ac.id>
2013-03-12 22:43:24 +0700

    Koneksi Basis Data v0.1
    Sementara seperti ini dulu ya.
    Belom sempat baca lagi.
    Dituggu komentar nya. Thanks

--- src/classes/App.php
+++ src/classes/App.php
@@ -163,4 +163,25 @@ class App {
 	protected function calendar() {
 		$this->loadTemplate('calendar');
 	}
+        
+        public function openDBconnection() {
+            // Create connection
+            $con=mysqli_connect('localhost','progin','progin','progin_405_13510033');
+            // Check connection
+            if (mysqli_connect_errno($con)) 
+            {
+                echo 'Failed to connect to MySQL:' . mysqli_connect_error();
+            }
+        }
+        
+        public function closeDBconnection(){
+            // Create connection
+            $con=mysqli_connect('localhost','progin','progin','progin_405_13510033');
+            // Check connection
+            if (mysqli_connect_errno($con)) 
+            {
+                echo 'Failed to connect to MySQL:' . mysqli_connect_error();
+            }
+            mysqli_close($con);
+        }
 }
