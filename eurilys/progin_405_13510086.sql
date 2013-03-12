de5e70ecdb3132e500c7ac7d5f85eac45423d489
Sharon Loh <lohh.sharon@gmail.com>
2013-03-12 20:44:24 +0700

    Update database sedikit.

--- eurilys/progin_405_13510086.sql
+++ eurilys/progin_405_13510086.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.4
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 12, 2013 at 04:47 AM
--- Server version: 5.1.44
--- PHP Version: 5.3.1
+-- Waktu pembuatan: 12. Maret 2013 jam 14:36
+-- Versi Server: 5.5.16
+-- Versi PHP: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,35 +19,36 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `progin_405_13510086`
 --
-
+CREATE DATABASE IF NOT EXISTS progin_405_13510086;
+USE progin_405_13510086;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ATTACHMENT`
+-- Struktur dari tabel `attachment`
 --
 
-CREATE TABLE IF NOT EXISTS `ATTACHMENT` (
+CREATE TABLE IF NOT EXISTS `attachment` (
   `att_id` int(11) NOT NULL AUTO_INCREMENT,
   `att_content` text NOT NULL,
-  `task_id` int(11) NOT NULL,
+  `att_task_id` int(11) NOT NULL,
   PRIMARY KEY (`att_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `ATTACHMENT`
+-- Dumping data untuk tabel `attachment`
 --
 
-INSERT INTO `ATTACHMENT` (`att_id`, `att_content`, `task_id`) VALUES
+INSERT INTO `attachment` (`att_id`, `att_content`, `att_task_id`) VALUES
 (1, 'dsadasdsdadsadasds', 1),
 (2, 'sadasdasdsaddsdas', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CATEGORY`
+-- Struktur dari tabel `category`
 --
 
-CREATE TABLE IF NOT EXISTS `CATEGORY` (
+CREATE TABLE IF NOT EXISTS `category` (
   `cat_id` int(11) NOT NULL AUTO_INCREMENT,
   `cat_name` varchar(50) NOT NULL,
   `cat_creator` varchar(50) NOT NULL,
@@ -56,40 +58,39 @@ CREATE TABLE IF NOT EXISTS `CATEGORY` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
--- Dumping data for table `CATEGORY`
+-- Dumping data untuk tabel `category`
 --
 
-INSERT INTO `CATEGORY` (`cat_id`, `cat_name`, `cat_creator`) VALUES
+INSERT INTO `category` (`cat_id`, `cat_name`, `cat_creator`) VALUES
 (1, 'Schoolicious', 'kennyazrina'),
 (2, 'Household', 'sharonloh');
-(3, 'BeautyStuff', 'nflubis');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CAT_ASIGNEE`
+-- Struktur dari tabel `cat_asignee`
 --
 
-CREATE TABLE IF NOT EXISTS `CAT_ASIGNEE` (
+CREATE TABLE IF NOT EXISTS `cat_asignee` (
   `cat_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `CAT_ASIGNEE`
+-- Dumping data untuk tabel `cat_asignee`
 --
 
-INSERT INTO `CAT_ASIGNEE` (`cat_id`, `username`) VALUES
+INSERT INTO `cat_asignee` (`cat_id`, `username`) VALUES
 (1, 'nflubis'),
 (1, 'sharonloh');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `COMMENT`
+-- Struktur dari tabel `comment`
 --
 
-CREATE TABLE IF NOT EXISTS `COMMENT` (
+CREATE TABLE IF NOT EXISTS `comment` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `comment_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `comment_content` text NOT NULL,
@@ -100,39 +101,39 @@ CREATE TABLE IF NOT EXISTS `COMMENT` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `COMMENT`
+-- Dumping data untuk tabel `comment`
 --
 
-INSERT INTO `COMMENT` (`comment_id`, `comment_timestamp`, `comment_content`, `task_id`, `comment_creator`) VALUES
-(1, '2013-03-12 11:44:26', 'asdasdasdas', 1, 'sharonloh'),
-(2, '2013-03-12 11:44:26', 'sndajasndnsajkdans', 1, 'nflubis');
+INSERT INTO `comment` (`comment_id`, `comment_timestamp`, `comment_content`, `task_id`, `comment_creator`) VALUES
+(1, '2013-03-12 04:44:26', 'asdasdasdas', 1, 'sharonloh'),
+(2, '2013-03-12 04:44:26', 'sndajasndnsajkdans', 1, 'nflubis');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TAG`
+-- Struktur dari tabel `tag`
 --
 
-CREATE TABLE IF NOT EXISTS `TAG` (
+CREATE TABLE IF NOT EXISTS `tag` (
   `tag_name` varchar(50) NOT NULL,
   `task_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `TAG`
+-- Dumping data untuk tabel `tag`
 --
 
-INSERT INTO `TAG` (`tag_name`, `task_id`) VALUES
+INSERT INTO `tag` (`tag_name`, `task_id`) VALUES
 ('capek', 1),
 ('males', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TASK`
+-- Struktur dari tabel `task`
 --
 
-CREATE TABLE IF NOT EXISTS `TASK` (
+CREATE TABLE IF NOT EXISTS `task` (
   `task_id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(50) NOT NULL,
   `task_status` tinyint(1) NOT NULL DEFAULT '0',
@@ -144,29 +145,29 @@ CREATE TABLE IF NOT EXISTS `TASK` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `TASK`
+-- Dumping data untuk tabel `task`
 --
 
-INSERT INTO `TASK` (`task_id`, `task_name`, `task_status`, `task_deadline`, `cat_name`, `task_creator`) VALUES
+INSERT INTO `task` (`task_id`, `task_name`, `task_status`, `task_deadline`, `cat_name`, `task_creator`) VALUES
 (1, 'Tubes Progin 2', 0, '2013-03-23', 'Schoolicious', 'kennyazrina'),
 (2, 'Tubes Progin 3', 0, '2013-04-25', 'Schoolicious', 'nflubis');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TASK_ASIGNEE`
+-- Struktur dari tabel `task_asignee`
 --
 
-CREATE TABLE IF NOT EXISTS `TASK_ASIGNEE` (
+CREATE TABLE IF NOT EXISTS `task_asignee` (
   `task_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `TASK_ASIGNEE`
+-- Dumping data untuk tabel `task_asignee`
 --
 
-INSERT INTO `TASK_ASIGNEE` (`task_id`, `username`) VALUES
+INSERT INTO `task_asignee` (`task_id`, `username`) VALUES
 (1, 'nflubis'),
 (2, 'sharonloh'),
 (1, 'sharonloh');
@@ -174,10 +175,10 @@ INSERT INTO `TASK_ASIGNEE` (`task_id`, `username`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `USER`
+-- Struktur dari tabel `user`
 --
 
-CREATE TABLE IF NOT EXISTS `USER` (
+CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
@@ -188,26 +189,30 @@ CREATE TABLE IF NOT EXISTS `USER` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `USER`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `USER` (`username`, `password`, `full_name`, `birthdate`, `avatar`, `email`) VALUES
+INSERT INTO `user` (`username`, `password`, `full_name`, `birthdate`, `avatar`, `email`) VALUES
 ('kennyazrina', 'kennyazrina', 'Kania Azrina', '1992-03-13', 'abcdefg', 'kaniaazrina@gmail.com'),
 ('nflubis', 'nflubis', 'Nurul Fithria Lubis', '1992-04-17', 'dasjndjksndkas', 'nflubis@gmail.com'),
 ('sharonloh', 'sharonloh', 'Sharon Loh', '1992-02-10', 'dsddsdsa', 'sharonloh@gmail.com');
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `CATEGORY`
+-- Ketidakleluasaan untuk tabel `category`
 --
-ALTER TABLE `CATEGORY`
+ALTER TABLE `category`
   ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`cat_creator`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `COMMENT`
+-- Ketidakleluasaan untuk tabel `comment`
 --
-ALTER TABLE `COMMENT`
+ALTER TABLE `comment`
   ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
