37ed5b39612f5ff4cc77a2d8d6119439f26707e9
Kevin Winata <kevinwinata@gmail.com>
2013-03-12 13:45:40 +0700

    SQL jadi (fix)
    Yang sebelumnya salah upload

--- progin_405_13510057.sql
+++ progin_405_13510057.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 09, 2013 at 03:25 AM
+-- Generation Time: Mar 12, 2013 at 07:43 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -19,6 +19,77 @@ SET time_zone = "+00:00";
 --
 -- Database: `progin_405_13510057`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignee`
+--
+
+CREATE TABLE IF NOT EXISTS `assignee` (
+  `username` varchar(12) NOT NULL,
+  `idtugas` varchar(10) NOT NULL,
+  KEY `assignee_ibfk_1` (`idtugas`),
+  KEY `assignee_ibfk_2` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hak`
+--
+
+CREATE TABLE IF NOT EXISTS `hak` (
+  `username` varchar(12) NOT NULL,
+  `idkategori` varchar(10) NOT NULL,
+  KEY `hak_ibfk_1` (`username`),
+  KEY `hak_ibfk_2` (`idkategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `idkategori` varchar(10) NOT NULL,
+  `namakategori` varchar(20) NOT NULL,
+  PRIMARY KEY (`idkategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`) VALUES
+('1', 'Kuliah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+
+CREATE TABLE IF NOT EXISTS `komentar` (
+  `idtugas` varchar(10) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `isikomentar` varchar(100) NOT NULL,
+  KEY `komentar_ibfk_1` (`username`),
+  KEY `komentar_ibfk_2` (`idtugas`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `idtugas` varchar(10) NOT NULL,
+  `isitag` varchar(20) NOT NULL,
+  KEY `tag_ibfk_1` (`idtugas`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -39,8 +110,66 @@ CREATE TABLE IF NOT EXISTS `tugas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tugas`
+--
+
+INSERT INTO `tugas` (`idtugas`, `namatugas`, `attachment`, `deadline`, `idkategori`, `username`) VALUES
+('1', 'tugas sister', '', '2013-03-08', '1', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `fullname` varchar(25) NOT NULL,
+  `birthdate` date NOT NULL,
+  `phonenumber` varchar(12) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `fullname`, `birthdate`, `phonenumber`, `email`) VALUES
+('admin', 'admin', 'Admin Z', '2013-03-08', '081977972383', 'admin@gmail.com');
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `assignee`
+--
+ALTER TABLE `assignee`
+  ADD CONSTRAINT `assignee_ibfk_1` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`idtugas`),
+  ADD CONSTRAINT `assignee_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `hak`
+--
+ALTER TABLE `hak`
+  ADD CONSTRAINT `hak_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `hak_ibfk_2` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`);
+
+--
+-- Constraints for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `komentar_ibfk_2` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`idtugas`);
+
+--
+-- Constraints for table `tag`
+--
+ALTER TABLE `tag`
+  ADD CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`idtugas`);
 
 --
 -- Constraints for table `tugas`
