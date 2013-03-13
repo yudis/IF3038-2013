76c070c112e684142a94e9b298b3a56b22519b09
Kevin Winata <kevinwinata@gmail.com>
2013-03-13 15:39:50 +0700

    Attachment + User (SQL)

--- progin_405_13510057.sql
+++ progin_405_13510057.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 12, 2013 at 05:35 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: Mar 13, 2013 at 08:38 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -38,6 +39,18 @@ CREATE TABLE IF NOT EXISTS `assignee` (
 
 INSERT INTO `assignee` (`username`, `idtugas`) VALUES
 ('iLol', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attachment`
+--
+
+CREATE TABLE IF NOT EXISTS `attachment` (
+  `idtugas` varchar(10) NOT NULL,
+  `isiattachment` varchar(20) NOT NULL,
+  KEY `attacment_ibfk_1` (`idtugas`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -94,11 +107,6 @@ CREATE TABLE IF NOT EXISTS `komentar` (
   KEY `komentar_ibfk_1` (`username`),
   KEY `komentar_ibfk_2` (`idtugas`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `komentar`
---
-
 
 -- --------------------------------------------------------
 
@@ -172,8 +180,17 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `fullname`, `birthdate`, `phonenumber`, `email`, `avatar`) VALUES
+('12345', '12345', 'abc def', '1900-01-01', '0', 'abc@gmail.com', ''),
+('borju', '12345', 'Wilson Fonda', '1990-01-01', '0', '222@gmail.com', ''),
+('dragoon20', '12345', 'Jordan Fernando', '1990-01-01', '0', 'dragoon@gmail.com', ''),
+('edo', '12345', 'Edward Samuel', '1990-01-01', '0', '555@gmail.com', ''),
+('excalibur', '12345', 'Mario Orlando Tank', '1990-01-01', '0', 'excalibur@yahoo.com', ''),
 ('iLol', '87654321', 'Kevin Winata', '1992-12-25', '081987654321', 'kevin@gmail.com', 'upload/kevin.jpg'),
-('moonray', '12345678', 'Raymond Lukanta', '1992-12-01', '081912345678', 'raymond@gmail.com', 'upload/raymond.jpg');
+('maria', '12345', 'Maria Orlanda', '1990-01-01', '0', 'maria@gmail.com', ''),
+('moonray', '12345678', 'Raymond Lukanta', '1992-12-01', '081912345678', 'raymond@gmail.com', 'upload/raymond.jpg'),
+('sun', '12345', 'Nikodemus Adriel', '1990-01-01', '0', '444@gmail.com', ''),
+('xelif', '12345', 'Felix Terahadi', '1990-01-01', '0', '333@gmail.com', ''),
+('yagami', '12345', 'Alfianto Jangtjik', '1990-01-01', '0', '111@gmail.com', '');
 
 --
 -- Constraints for dumped tables
@@ -185,6 +202,12 @@ INSERT INTO `user` (`username`, `password`, `fullname`, `birthdate`, `phonenumbe
 ALTER TABLE `assignee`
   ADD CONSTRAINT `assignee_ibfk_1` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`idtugas`),
   ADD CONSTRAINT `assignee_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `attachment`
+--
+ALTER TABLE `attachment`
+  ADD CONSTRAINT `attacment_ibfk_1` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`idtugas`);
 
 --
 -- Constraints for table `hak`
@@ -212,3 +235,7 @@ ALTER TABLE `tag`
 ALTER TABLE `tugas`
   ADD CONSTRAINT `tugas_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
   ADD CONSTRAINT `tugas_ibfk_2` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
