bc7ed888b2b8e6c5c4de760c945697d70a9bb301
Adriel <a.limanthie@yahoo.com>
2013-03-14 21:31:29 +0700

    mengubah tampilan dan fungsi profile.html sesuai dengan spek

--- Adriel/scripts/profile.js
+++ Adriel/scripts/profile.js
@@ -1,9 +1,52 @@
+function checkChange()
+{
+	var flag = true;
+
+	if (document.getElementById("nameDisplayDiv").innerHTML == document.getElementById("fullname").value)
+	{
+		if (document.getElementById("emailDisplayDiv").innerHTML == document.getElementById("email").value)
+		{
+			if (document.getElementById("dateDisplayDiv").innerHTML == document.getElementById("date").value)
+			{
+				if (document.getElementById("majorDisplayDiv").innerHTML == document.getElementById("major").value)
+				{
+					if (document.getElementById("majorDisplayDiv").innerHTML == document.getElementById("major").value)
+					{
+						
+					}
+					else
+					{
+						flag = false;
+					}
+				}
+				else
+				{
+					flag = false;
+				}
+			}
+			else
+			{
+				flag = false;
+			}
+		}
+		else
+		{
+			flag = false;
+		}
+	}
+	else
+	{
+		flag = false;
+	}
+}
+
 function editProfil()
 {
 	document.getElementById("nameEditDiv").style.display = "inline";
 	document.getElementById("emailEditDiv").style.display = "inline";
 	document.getElementById("dateEditDiv").style.display = "inline";
 	document.getElementById("majorEditDiv").style.display = "inline";
+	document.getElementById("passwordEditDiv").style.display = "inline";
 	document.getElementById("doneProfileButton").style.display = "inline";
 	document.getElementById("changeProfilePictureButton").style.display = "inline";
 	
@@ -18,10 +61,13 @@ function editProfil()
 
 function doneProfil()
 {
+	checkChange();
+
 	document.getElementById("nameEditDiv").style.display = "none";
 	document.getElementById("emailEditDiv").style.display = "none";
 	document.getElementById("dateEditDiv").style.display = "none";
 	document.getElementById("majorEditDiv").style.display = "none";
+	document.getElementById("passwordEditDiv").style.display = "none";
 	document.getElementById("doneProfileButton").style.display = "none";
 	document.getElementById("changeProfilePictureButton").style.display = "none";
 	
