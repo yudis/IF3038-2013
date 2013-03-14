d55ae06d25ab1938b7e0465067dea84c231d9bb2
marioorlando <mario_orlando_live.com>
2013-03-15 00:31:18 +0700

    Menambahkan fitur profile, masih kurang check edit profile

---
+++ Progin2/editprofile.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if(!isset($_SESSION['id']))
+  header("location:index.html");
+
+$con = mysql_connect("localhost:3306","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("progin_405_13510057", $con);
+
+if (!empty($_POST['nama_lengkap'])) {
+	echo "wkwkwkw";
+	$fullname = $_POST['nama_lengkap'];
+	echo $fullname;
+	mysql_query("UPDATE user SET fullname='$fullname' WHERE username='$_SESSION[id]'");
+}
+
+if (!empty($_POST['password'])) {
+	echo "hahahaha";
+	$password = $_POST['password'];
+	mysql_query("UPDATE user SET password='$password' WHERE username='$_SESSION[id]'");
+}
+
+if (($_POST['bulan']) != '--Bulan--') {
+	echo "lololol";
+	$birthdate = date("Y/m/d", strtotime(($_POST['tanggal'])." ".($_POST['bulan'])." ".($_POST['tahun'])));
+	mysql_query("UPDATE user SET birthdate='$birthdate' WHERE username='$_SESSION[id]'");
+}
+
+if (!empty($_FILES["avatar"]["name"])) {
+	echo "wkwoakwoakw";
+	$avatar = ("upload/" . $_FILES["avatar"]["name"]);
+	mysql_query("UPDATE user SET avatar='$avatar' WHERE username='$_SESSION[id]'");
+	
+	move_uploaded_file($_FILES["avatar"]["tmp_name"], "upload/" . $_FILES["avatar"]["name"]);
+
+}
+
+header("location:profile.php");
+
+mysql_close($con);
+?>
--- Progin2/profile.php
+++ Progin2/profile.php
@@ -5,7 +5,111 @@
 <link href="styles/header.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
+<script>
+	function makeTgl(){
+		for(var i=1; i<=31; i++){
+			var isi=document.createTextNode(i);
+			var opsi = document.createElement("option");
+			opsi.setAttribute("value",i);
+			opsi.appendChild(isi);
+			document.getElementById("tgl").appendChild(opsi);
+		}
+	}
+</script>
+
+<script>
+	function makeThn(){
+		for(var i=1955; i<=2013; i++){
+			var isi=document.createTextNode(i);
+			var opsi = document.createElement("option");
+			opsi.setAttribute("value",i);
+			opsi.appendChild(isi);
+			document.getElementById("thn").appendChild(opsi);
+		}
+	}
+</script>
+
+<script>
+	function validateFullName(nama) {
+			var str = nama;
+			var idx = str.indexOf(' ');
+			if (idx > 0 && idx < str.length - 1) {
+				var chr1 = str.charAt(idx - 1);
+				var chr2 = str.charAt(idx + 1);
+				if (chr1 != ' ' && chr2 != ' ') {
+					document.getElementById('v_nama').innerHTML='<font color="green">Benar</font>';
+					return true;	
+				} else {
+					document.getElementById('v_nama').innerHTML='<font color="red">Nama lengkap minimal 2 kata dipisahkan 1 spasi</font>'; 
+				return false;
+				}
+			} else {
+				document.getElementById('v_nama').innerHTML='<font color="red">Nama lengkap minimal 2 kata dipisahkan 1 spasi</font>'; 
+				return false;
+			}
+	}
+</script>
+
+<script>
+	function checkPass(pass, uname, email){
+		if((pass != uname) && (pass != email)){
+			document.getElementById('v_pass').innerHTML='<font color="green">Benar</font>';
+			return true;					
+		}else if(pass == uname){
+			document.getElementById('v_pass').innerHTML='<font color="red">Password tidak boleh sama dengan username</font>'; 
+			return false;
+		}
+		else if(pass == email){
+			document.getElementById('v_pass').innerHTML='<font color="red">Password tidak boleh sama dengan email</font>'; 
+			return false;
+		}
+		else{
+			document.getElementById('v_pass').innerHTML='<font color="red">Password tidak boleh sama dengan username/email</font>'; 
+			return false;
+		}
+	}
+</script>
+
+<script>
+	function checkCPass(cpass, pass){
+		if(cpass == pass){
+			document.getElementById('v_cpass').innerHTML='<font color="green">Benar</font>';
+			return true;					
+		}else{
+			document.getElementById('v_cpass').innerHTML='<font color="red">Salah</font>'; 
+			return false;
+		}
+	}
+</script>
+
+<script>
+	function validateAvatar(avatar) {
+		var str = avatar;
+		var ext = str.substring(str.lastIndexOf('.') + 1, str.length);
+		if (ext.toLowerCase() == "jpeg" || ext.toLowerCase() == "jpg") {
+			document.getElementById('v_avatar').innerHTML='<font color="green">Benar</font>';
+			return true;	
+		} else {
+			document.getElementById('v_avatar').innerHTML='<font color="red">Avatar harus ekstensi .jpg atau .jpeg</font>'; 
+			return false;
+		}
+	}
+</script>
+
+<script>
+	function check(password, cpassword) {
+		if (password == cpassword) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+</script>
+
+<body onLoad="makeTgl();makeThn();">
+	<?php
+	session_start();
+	?>
 	<div id="container">
 		<div id="header">
         	<div class=logo id="logo">
@@ -39,10 +143,9 @@
 		<div id="profilearea">
 			<div class="profilephoto">
 				<?php
-				session_start();
 				if(!isset($_SESSION['id']))
-				  header("location:index.html");
-				  
+					header("location:index.html");
+	
 				$con = mysql_connect("localhost:3306","root","");
 				if (!$con)
 				  {
@@ -68,9 +171,8 @@
 					echo "Handphone : ".$row['phonenumber']."<br><br>";
 					echo "Email	  	: ".$row['email']."<br>";
 				}
-				mysql_close($con);
 				?>
-				<button type="button" id="editbutton"></button>
+				<a href="#editprofile_form"><button type="button" id="editbutton"></button></a>
 			</div>
 		</div>
 		<div id="listarea">
@@ -81,129 +183,116 @@
 				Done Task
 			</div>
         	<div id="undonetask">
-				<div class="task_block" id="k11">
-					<div class="task_judul">
-						Cari Buku
-					</div>
-					<div class="task_deadline">
-						Deadline: 21 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah
-					</div>
-				</div>
-				
-				<div class="task_block" id="k12">
-					<div class="task_judul">
-						Tugas Intelegensia Buatan
-					</div>
-					<div class="task_deadline">
-						Deadline: 22 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah, IB, tugas
-					</div>
-				</div>
-		
-				<div class="task_block" id="k21">
-					<div class="task_judul">
-						Cari Monyet
-					</div>
-					<div class="task_deadline">
-						Deadline: 24 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: eksperimen
-					</div>
-				</div>
-				
-				<div class="task_block" id="k22">
-					<div class="task_judul">
-						Beli Formalin
-					</div>
-					<div class="task_deadline">
-						Deadline: 25 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: eksperimen
-					</div>
-				</div>
-				
-				<div class="task_block" id="k13">
-					<div class="task_judul">
-						Tugas IMK
-					</div>
-					<div class="task_deadline">
-						Deadline: 25 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah, IMK, tugas
-					</div>
-				</div>
-				
-				<div class="task_block" id="k14">
-					<div class="task_judul">
-						Kuis IMK
-					</div>
-					<div class="task_deadline">
-						Deadline: 28 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah, IMK, kuis
-					</div>
-				</div>
-				
-				<div class="task_block" id="k31">
-					<div class="task_judul">
-						Latihan Silat
-					</div>
-					<div class="task_deadline">
-						Deadline: 28 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: bela diri
-					</div>
-				</div>
-	
+				<?php
+				$result = mysql_query("SELECT * FROM tugas WHERE username='$_SESSION[id]' AND status='undone'");
+				while($row = mysql_fetch_array($result)) {
+					echo "<div class=\"task_block\">";
+					echo 	"<div class=\"task_judul\">";
+					echo 	$row['namatugas'];
+					echo 	"</div>";
+					echo 	"<div class=\"task_deadline\">";
+					echo	"Deadline: ".date('d F Y',strtotime($row['deadline']));
+					echo	"</div>";
+					echo	"<div class=\"task_tag\">";
+					echo	"Tags: ";
+					$result2 = mysql_query("SELECT * FROM tag WHERE idtugas='$row[idtugas]'");
+					$count = mysql_num_rows($result2);
+					while($row2 = mysql_fetch_array($result2)) {
+						if ($count == 1) {
+							echo $row2['isitag'];
+						} else {
+							echo $row2['isitag'].", ";
+							$count--;
+						}
+					}
+					echo	"</div>";
+					echo "</div>";
+				}
+				?>
 			</div>
 			<div id="donetask">
-				<div class="task_block" id="k41">
-					<div class="task_judul">
-						Ngulang Pelajaran
-					</div>
-					<div class="task_deadline">
-						Deadline: 12 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah
-					</div>
-				</div>
-				
-				<div class="task_block" id="k42">
-					<div class="task_judul">
-						Beli Racun
-					</div>
-					<div class="task_deadline">
-						Deadline: 15 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: eksperimen
-					</div>
-				</div>
-		
-				<div class="task_block" id="k43">
-					<div class="task_judul">
-						Belajar Progin
-					</div>
-					<div class="task_deadline">
-						Deadline: 18 February 2013
-					</div>
-					<div class="task_tag">
-						Tags: kuliah
-					</div>
-				</div>
+				<?php
+				$result = mysql_query("SELECT * FROM tugas WHERE username='$_SESSION[id]' AND status='done'");
+				while($row = mysql_fetch_array($result)) {
+					echo "<div class=\"task_block\">";
+					echo 	"<div class=\"task_judul\">";
+					echo 	$row['namatugas'];
+					echo 	"</div>";
+					echo 	"<div class=\"task_deadline\">";
+					echo	"Deadline: ".date('d F Y',strtotime($row['deadline']));
+					echo	"</div>";
+					echo	"<div class=\"task_tag\">";
+					echo	"Tags: ";
+					$result2 = mysql_query("SELECT * FROM tag WHERE idtugas='$row[idtugas]'");
+					$count = mysql_num_rows($result2);
+					while($row2 = mysql_fetch_array($result2)) {
+						if ($count == 1) {
+							echo $row2['isitag'];
+						} else {
+							echo $row2['isitag'].", ";
+							$count--;
+						}
+					}
+					echo	"</div>";
+					echo "</div>";
+				}
+				?>
 			</div>
-        </div>
-	</div>
+		</div>
+		
+		<!--Popup edit profile -->
+		<a href="#x" class="overlay" id="editprofile_form"></a>
+		<div class="popup">
+			<form name="editprofile" method="post" onSubmit="return check(document.editprofile.password.value, document.editprofile.confirm_password.value)" action="editprofile.php" enctype="multipart/form-data">
+				Fullname : <input name="nama_lengkap" id="nama_lengkap" type="text" maxlength="256" onKeyUp="validateFullName(document.editprofile.nama_lengkap.value)"><br>
+				<div id="v_nama">
+				</div>
+				Password : <input name="password" type="password" maxlength="24" onKeyUp="checkPass(document.editprofile.password.value,
+					<?php
+					echo "'".$_SESSION['id']."'";
+					?>
+					,
+					<?php
+					$result = mysql_query("SELECT * FROM user WHERE username='$_SESSION[id]'");
+					while($row = mysql_fetch_array($result)) {
+						echo "'".$row['email']."'";
+					}
+					?>
+				)"> <br>
+				<div id="v_pass">
+				</div>
+				Confirm Password : <input name="confirm_password" type="password" maxlength="24" onKeyUp="checkCPass(document.editprofile.confirm_password.value, document.editprofile.password.value)"><br>
+				<div id="v_cpass">
+				</div>
+				Birthdate : 
+					<select name="tanggal" id="tgl">
+						<option>--Tanggal--</option>
+					</select>
+					<select name="bulan">
+						<option>--Bulan--</option>
+						<option value="January">January</option>
+							<option value="February">February</option>
+							<option value="March">March</option>
+							<option value="April">April</option>
+							<option value="May">May</option>
+							<option value="June">June</option>
+							<option value="July">July</option>
+							<option value="August">August</option>
+							<option value="September">September</option>
+							<option value="October">October</option>
+							<option value="November">November</option>
+							<option value="December">December</option>
+					</select>
+					<select name="tahun" id="thn">
+						<option>--Tahun--</option>
+					</select>
+				<br>
+				Avatar : <input type="file" name="avatar"><br>
+				<div id="v_avatar">
+				</div>
+				<input type="submit" name="submit" value="Edit">
+			<a class="close" href="#"></a>
+			</form>
+		</div>
 </body>
 </html>
