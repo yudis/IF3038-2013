8d29d5fbfec9128bd1252c1001bdce465880e3c1
Kevin Winata <kevinwinata@gmail.com>
2013-03-15 13:49:40 +0700

    File yang ketinggalan

---
+++ Progin2/changestatus.php
@@ -0,0 +1,23 @@
+<?php
+$con = mysql_connect("localhost:3306","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("progin_405_13510057", $con);
+
+$idtugas = $_GET["q"];
+$idkategori = $_GET["p"];
+
+$result = mysql_query("SELECT status FROM tugas WHERE idtugas = '$idtugas'");
+$row = mysql_fetch_array($result);
+if ($row['status'] == done)
+	mysql_query("UPDATE tugas SET status='undone' WHERE idtugas = '$idtugas'");
+else 
+	mysql_query("UPDATE tugas SET status='done' WHERE idtugas = '$idtugas'");
+	
+mysql_close($con);
+
+header("location:dashboard.php?q=".$idkategori);
+?>
---
+++ Progin2/viewtask.php
@@ -0,0 +1,298 @@
+<html>
+	<head>
+		<title>View Task</title>
+		<link href="styles/viewtask.css" rel="stylesheet" type="text/css" />
+		<link href="styles/header.css" rel="stylesheet" type="text/css" />
+		<script>
+			var attach = "videos/attachmentsample.ogg";
+			var tanggal = 1;
+			var bulan = "Januari";
+			var tahun = 2013;
+			
+			function makeTgl(){
+				for(var i=1; i<=31; i++){
+					var isi=document.createTextNode(i);
+					var opsi = document.createElement("option");
+					opsi.setAttribute("value",i);
+					opsi.appendChild(isi);
+					document.getElementById("tgl").appendChild(opsi);
+				}
+			}
+				
+			function makeThn(){
+				for(var i=1955; i<=2013; i++){
+					var isi=document.createTextNode(i);
+					var opsi = document.createElement("option");
+					opsi.setAttribute("value",i);
+					opsi.appendChild(isi);
+					document.getElementById("thn").appendChild(opsi);
+				}
+			}
+			
+			function getStyle(el, name)
+			{
+			  if ( document.defaultView && document.defaultView.getComputedStyle )
+			  {
+				var style = document.defaultView.getComputedStyle(el, null);
+				if ( style )
+				  return style[name];
+			  }
+			  else if ( el.currentStyle )
+				return el.currentStyle[name];
+
+			  return null;
+			}
+
+			function showHide(a){
+			  var e=document.getElementById(a);
+			  if(!e)return true;
+			  if(getStyle(e, "display") == "none"){
+				e.style.display="inline"
+			  } else {
+				e.style.display="none"
+			  }
+			  return true;
+			}
+			
+			function renameButton(id){
+				var elem = document.getElementById(id);
+				if (elem.value=="Edit") elem.value = "Cancel";
+				else elem.value = "Edit";
+			}
+			
+			function clearContents(id) {
+				document.getElementById(id).value = '';
+			}
+			
+			function editValue(id1,id2){
+				var value = document.getElementById(id1).value;
+				document.getElementById(id2).innerHTML = value;
+			}
+			
+			function setDateValue() {
+				document.getElementById("dlvalue").innerHTML = tanggal+"-"+bulan+"-"+tahun;
+			}
+			
+			function getDateValue() {
+				tanggal = document.edit.tgl.value;
+				bulan = document.edit.bln.value;
+				tahun = document.edit.thn.value;
+			}
+			
+			function checkEdit(e,id1,id2,id3) {
+				if (e && e.keyCode == 13) {
+					editValue(id1,id2);
+					showHide(id1);
+					showHide(id2);
+					renameButton(id3);
+				}
+			}
+			
+			function checkSubmit(e) {
+				if (e && e.keyCode == 13) {
+					displayComment();
+				}
+			}
+			
+			function displayComment(){
+				var newpara = document.createElement("P")
+				text = document.createTextNode(commentField.value);
+				newpara.appendChild(text)
+				document.getElementById("comment").appendChild(newpara);
+				commentField.value = '';
+			}
+			
+			</script>
+	</head>
+
+	<body onload="makeTgl();makeThn()">
+		<div id="container">
+			<div id="header">
+				<div class=logo id="logo">
+					<a href="dashboard.html"><img src="images/logo.png" title="Home" alt="Home"/></a>
+				</div>
+				<div id="space">
+				</div>
+				<div class = "menu" id = "search">
+				 <form name="search" method="post" action="search.php">
+					 Search for: <input type="text" name="find" /> in 
+					 <Select NAME="field">
+					 <Option VALUE="semua">Semua</option>
+					 <Option VALUE="username">Username</option>
+					 <Option VALUE="namakategori">Judul Kategori</option>
+					 <Option VALUE="tasktag">Task atau Tag</option>
+					 </Select>
+					 <input type="hidden" name="searching" value="yes" />
+					 <button type="submit" id="searchbutton"></button>
+				 </form>
+			</div>
+				<div class="menu" id="logout" action="logout.php">
+					<a href="index.html">Logout</a>
+				</div>
+				<div class="menu" id="profile">
+					<a href="profile.php">Profile</a>
+				</div>
+				<div class="menu" id="home">
+					<a href="dashboard.php">Home</a>
+				</div>
+			</div>
+			<div id="leftspace">
+				
+			</div>
+			<div id="viewtask">
+				<form name=edit>
+					<div class="form_field">
+						<div class="viewtask_label">
+							Task Name
+						</div>
+						<div class="viewtask_field">
+							<?php
+							$con = mysql_connect("localhost:3306","root","");
+							if (!$con)
+							  {
+							  die('Could not connect: ' . mysql_error());
+							  }
+
+							mysql_select_db("progin_405_13510057", $con);
+							$idtugas = $_GET["q"];
+							
+							$tugas = mysql_query("SELECT * FROM tugas WHERE idtugas='$idtugas'");
+							$row = mysql_fetch_array($tugas);
+							echo $row['namatugas'];
+							?>
+						</div>
+					</div>
+					<div class="form_attachment">
+					</div>
+					<div class="form_field">
+						<div class="viewtask_label">
+							Attachment
+						</div>
+						<div class="viewtask_field">
+							<?php
+							$attachment = mysql_query("SELECT isiattachment FROM attachment WHERE idtugas='$idtugas'");
+							while($row = mysql_fetch_array($attachment))
+								echo "<a id=link href=\"".$row['isiattachment']."\" target=\"_blank\">".$row['isiattachment']."</a>";
+							?>
+						</div>
+					</div>
+					<div class="form_field">
+						<div class="viewtask_label">
+							Deadline
+						</div>
+						<div class="viewtask_field">
+							<div>
+								<p id="dlvalue">
+								<?php
+								$tugas = mysql_query("SELECT * FROM tugas WHERE idtugas='$idtugas'");
+								$row = mysql_fetch_array($tugas);
+								echo $row['deadline'];
+								?>
+								</p>
+							</div>
+							<div id="dl">
+								<select name="tanggal" id="tgl">
+								</select>
+								<select name="bulan" id="bln">
+									<option value="Januari">Januari</option>
+									<option value="Februari">Februari</option>
+									<option value="Maret">Maret</option>
+									<option value="April">April</option>
+									<option value="Mei">Mei</option>
+									<option value="Juni">Juni</option>
+									<option value="Juli">Juli</option>
+									<option value="Agustus">Agustus</option>
+									<option value="September">September</option>
+									<option value="Oktober">Oktober</option>
+									<option value="November">November</option>
+									<option value="Desember">Desember</option>
+								</select>
+								<select name="tahun" id="thn">
+								</select>
+							</div>
+						</div>
+						<div class="viewtask_edit">
+							<input type=button value="Edit" id="dlbutton" onClick="showHide('dl');showHide('dlvalue');showHide('dlbutton');showHide('savebutton')">
+							<input type=button value="Save" id="savebutton" onClick="showHide('dl');showHide('dlvalue');showHide('dlbutton');showHide('savebutton');getDateValue();setDateValue()">
+						</div>
+					</div>
+					<div class="form_field">
+						<div class="viewtask_label">
+							Assignee
+						</div>
+						<div class="viewtask_field">
+							<p id="asvalue">
+							<?php
+							$assignee = mysql_query("SELECT username FROM assignee WHERE idtugas='$idtugas'");
+							$count = mysql_num_rows($assignee);
+							while($row = mysql_fetch_array($assignee)){
+								if ($count > 1)
+									echo $row['username'].", ";
+								else 
+									echo $row['username'];
+								$count--;
+							}
+							?>
+							</p>
+							<input type=text name=as id="as" onKeyPress="checkEdit(event,'as','asvalue','asbutton')" list="suggest"/>
+							<datalist id="suggest">
+								<option value="Mario Orlando">
+								<option value="Luthfi Chandra">
+								<option value="Rubiano Adityas">
+							</datalist>
+							
+						</div>
+						<div class="viewtask_edit">
+							<input type=button value="Edit" id="asbutton" onClick="showHide('as');showHide('asvalue');renameButton('asbutton');clearContents('as')">
+						</div>
+						
+					</div>
+					<div class="form_field">
+						<div class="viewtask_label">
+							Tag
+						</div>
+						<div class="viewtask_field">
+							<p id="tagvalue">
+							<?php
+							$tag = mysql_query("SELECT isitag FROM tag WHERE idtugas='$idtugas'");
+							$count = mysql_num_rows($tag);
+							while($row = mysql_fetch_array($tag)){
+								if ($count > 1)
+									echo $row['isitag'].", ";
+								else 
+									echo $row['isitag'];
+								$count--;
+							}
+							?>
+							</p>
+							<input type=text name=tag id="tag" onKeyPress="checkEdit(event,'tag','tagvalue','tagbutton')">
+						</div>
+						<div class="viewtask_edit">
+							<input type=button value="Edit" id="tagbutton" onClick="showHide('tag');showHide('tagvalue');renameButton('tagbutton');clearContents('tag')">
+						</div>
+					</div>
+				</form>
+			</div>
+			<div id="rightspace">
+				<div>
+					<div>
+						<fieldset id="fieldset">
+							<legend id="commentLabel">Comment</legend>
+							<p id="comment">Deadlinenya sebentar lagi nih!</p>
+						</fieldset>
+					</div>
+				</div>
+				<div>
+					<div>
+						<textarea id="commentField" rows="3" cols="30" onKeyPress="checkSubmit(event)"></textarea>
+					</div>
+				</div>
+				<div>
+					<div>
+						<input type=button value="Submit Comment" onClick="displayComment()">
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
