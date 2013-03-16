f288148b1129d5fbce7c184c0debaccacecd3030
Abraham Krisnanda S <13510033@std.stei.itb.ac.id>
2013-03-17 00:31:28 +0700

    Basis Data v0.4
    Fix password attribute on user

--- sql/progin_405_13510033.sql
+++ sql/progin_405_13510033.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 16, 2013 at 05:55 PM
+-- Generation Time: Mar 16, 2013 at 06:29 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -50,13 +50,6 @@ CREATE TABLE IF NOT EXISTS `comment` (
   UNIQUE KEY `id_task` (`id_task`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`id_komentar`, `timestamp`, `komentar`, `id_user`, `id_task`) VALUES
-(1, '2013-03-15 10:43:22', 'wah tugasnya lumayan nih', 1, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -96,13 +89,6 @@ CREATE TABLE IF NOT EXISTS `have_task` (
   KEY `id_user` (`id_user`,`id_task`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `have_task`
---
-
-INSERT INTO `have_task` (`id_user`, `id_task`) VALUES
-(2, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -116,13 +102,6 @@ CREATE TABLE IF NOT EXISTS `kategori` (
   PRIMARY KEY (`id_kategori`),
   KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `kategori`
---
-
-INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `id_user`) VALUES
-(1, 'IF3038 - Pemrograman Internet', 1);
 
 -- --------------------------------------------------------
 
@@ -153,13 +132,6 @@ CREATE TABLE IF NOT EXISTS `task` (
   KEY `id_kategori` (`id_kategori`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `task`
---
-
-INSERT INTO `task` (`id_task`, `nama_task`, `status`, `deadline`, `id_kategori`) VALUES
-(2, 'Tugas2_PHP_Ajax', 0, '2013-03-23', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -185,17 +157,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `fullname` varchar(50) NOT NULL,
   `avatar` varchar(100) NOT NULL,
   `birthdate` date NOT NULL,
+  `password` varchar(50) NOT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id_user`, `username`, `e-mail`, `fullname`, `avatar`, `birthdate`) VALUES
-(1, 'abrahamks', 'abraham.k@students.itb.ac.id', 'Abraham Krisnanda', 'C:/asdf', '2013-02-08'),
-(2, 'admin', 'admin@yahoo.com', 'administrator ku', 'C:/asdf', '2013-03-01');
 
 --
 -- Constraints for dumped tables
@@ -235,12 +200,6 @@ ALTER TABLE `have_tags`
 ALTER TABLE `have_task`
   ADD CONSTRAINT `have_task_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
   ADD CONSTRAINT `have_task_ibfk_2` FOREIGN KEY (`id_task`) REFERENCES `task` (`id_task`);
-
---
--- Constraints for table `kategori`
---
-ALTER TABLE `kategori`
-  ADD CONSTRAINT `kategori_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 
 --
 -- Constraints for table `task`
