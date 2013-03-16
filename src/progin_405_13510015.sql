248ce7f4836e4c393d581c45e4c239075f265b0e
Edward Samuel <edward_samuel_esp@yahoo.com>
2013-03-16 13:43:08 +0700

    Perbaikan Basis Data - Penambahan tabel comments untuk menampung komentar tugas - Penghapusan tabel komentar, relationship digabungkan pada tabel tugas - Rename table asignee menjadi assignees

--- src/progin_405_13510015.sql
+++ src/progin_405_13510015.sql
@@ -1,149 +1,191 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.27, for Win32 (x86)
 --
--- Host: localhost
--- Generation Time: Mar 14, 2013 at 01:58 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: progin_405_13510015
+-- ------------------------------------------------------
+-- Server version	5.5.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `progin_405_13510015`
+-- Table structure for table `assignees`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `asignee`
---
-
-CREATE TABLE IF NOT EXISTS `asignee` (
-  `id_tugas` int(100) NOT NULL,
+DROP TABLE IF EXISTS `assignees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `assignees` (
+  `id_tugas` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
   PRIMARY KEY (`id_tugas`,`username`),
-  KEY `asignee_ibfk_2` (`username`)
+  KEY `asignee_ibfk_2` (`username`),
+  CONSTRAINT `assignees_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`),
+  CONSTRAINT `assignees_ibfk_2` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `assignees`
+--
+
+LOCK TABLES `assignees` WRITE;
+/*!40000 ALTER TABLE `assignees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assignees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `attachments`
 --
 
-CREATE TABLE IF NOT EXISTS `attachments` (
-  `id_tugas` int(100) NOT NULL,
+DROP TABLE IF EXISTS `attachments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attachments` (
+  `id_tugas` int(11) NOT NULL,
   `attachment` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_tugas`,`attachment`)
+  PRIMARY KEY (`id_tugas`,`attachment`),
+  CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `attachments`
+--
+
+LOCK TABLES `attachments` WRITE;
+/*!40000 ALTER TABLE `attachments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attachments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id_tugas` int(100) NOT NULL,
-  `id_kategori` int(100) NOT NULL,
-  PRIMARY KEY (`id_tugas`,`id_kategori`),
-  KEY `tugas@002dkategori_ibfk_2` (`id_kategori`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `last_mod` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id_tugas`, `id_kategori`) VALUES
-(1, 1),
-(2, 2),
-(3, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kategori`
---
-
-CREATE TABLE IF NOT EXISTS `kategori` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(50) NOT NULL,
-  `last_mod` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Intelegensi buatan','2013-03-07 06:46:36'),(2,'Progin','2013-03-12 10:00:00'),(3,'IMK','2013-03-14 00:26:14'),(4,'SIster','2013-03-14 00:41:42'),(5,'Benny','2013-03-14 01:17:38');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Dumping data for table `kategori`
+-- Table structure for table `comments`
 --
 
-INSERT INTO `kategori` (`id`, `nama`, `last_mod`) VALUES
-(1, 'Intelegensi buatan', '2013-03-07 13:46:36'),
-(2, 'Progin', '2013-03-12 17:00:00'),
-(3, 'IMK', '2013-03-14 07:26:14'),
-(4, 'SIster', '2013-03-14 07:41:42'),
-(5, 'Benny', '2013-03-14 08:17:38');
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idtugas` int(11) NOT NULL,
+  `user` varchar(32) NOT NULL,
+  `time` int(11) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_ibfk_1` (`idtugas`),
+  KEY `comments_ibfk_2` (`user`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`idtugas`) REFERENCES `tugas` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tags`
 --
 
-CREATE TABLE IF NOT EXISTS `tags` (
-  `id_tugas` int(100) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id_tugas` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(30) NOT NULL,
-  PRIMARY KEY (`id_tugas`,`tag`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id_tugas`,`tag`),
+  CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tags`
 --
 
-INSERT INTO `tags` (`id_tugas`, `tag`) VALUES
-(1, 'ada'),
-(1, 'tes');
-
--- --------------------------------------------------------
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (1,'ada'),(1,'tes');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tugas`
 --
 
-CREATE TABLE IF NOT EXISTS `tugas` (
-  `id` int(100) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tugas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tugas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(50) NOT NULL,
   `tgl_deadline` date NOT NULL,
   `status` tinyint(1) NOT NULL,
   `last_mod` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `pemilik` varchar(32) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `pemilik` (`pemilik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  KEY `tugas_ibfk_1` (`pemilik`),
+  KEY `tugas_ibfk_2` (`id_kategori`),
+  CONSTRAINT `tugas_ibfk_1` FOREIGN KEY (`pemilik`) REFERENCES `users` (`username`),
+  CONSTRAINT `tugas_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tugas`
 --
 
-INSERT INTO `tugas` (`id`, `nama`, `tgl_deadline`, `status`, `last_mod`, `pemilik`) VALUES
-(1, 'gunBound', '2013-03-22', 0, '2013-03-12 17:00:00', 'edwardsp'),
-(2, 'to do list', '2013-03-22', 0, '2013-03-12 17:00:00', 'edogawa'),
-(3, 'gun', '2013-03-26', 1, '2013-03-13 17:00:00', 'edogawa');
-
--- --------------------------------------------------------
+LOCK TABLES `tugas` WRITE;
+/*!40000 ALTER TABLE `tugas` DISABLE KEYS */;
+INSERT INTO `tugas` VALUES (1,'gunBound','2013-03-22',0,'2013-03-12 10:00:00','edwardsp',1),(2,'to do list','2013-03-22',0,'2013-03-12 10:00:00','edogawa',2),(3,'gun','2013-03-26',1,'2013-03-13 10:00:00','edogawa',2);
+/*!40000 ALTER TABLE `tugas` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `username` varchar(32) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` char(32) NOT NULL,
@@ -151,55 +193,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `tgl_lahir` date NOT NULL,
   `avatar` varchar(50) NOT NULL,
   PRIMARY KEY (`username`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `email_2` (`email`),
-  UNIQUE KEY `email_3` (`email`)
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `email`, `password`, `full_name`, `tgl_lahir`, `avatar`) VALUES
-('edogawa', 'edward_samuel_esp@yahoo.com', '19b13f6bbc0d19a5dba84b209a22a487', 'Edward Samuel', '0000-00-00', 'image-1.jpg'),
-('edwardsp', 'edward_samuel_esp@hotmail.com', '9aa6e5f2256c17d2d430b100032b997c', 'Edward Samuel Pasaribu', '1992-07-16', 'edwardsp.jpg');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('edogawa','edward_samuel_esp@yahoo.com','19b13f6bbc0d19a5dba84b209a22a487','Edward Samuel','1991-12-16','edogawa.jpg'),('edwardsp','edward_samuel_esp@hotmail.com','9aa6e5f2256c17d2d430b100032b997c','Edward Samuel Pasaribu','1992-07-16','edwardsp.jpg');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `asignee`
---
-ALTER TABLE `asignee`
-  ADD CONSTRAINT `asignee_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `asignee_ibfk_2` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `attachments`
---
-ALTER TABLE `attachments`
-  ADD CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `categories`
---
-ALTER TABLE `categories`
-  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `categories_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tags`
---
-ALTER TABLE `tags`
-  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`id_tugas`) REFERENCES `tugas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tugas`
---
-ALTER TABLE `tugas`
-  ADD CONSTRAINT `tugas_ibfk_1` FOREIGN KEY (`pemilik`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-03-16 13:38:44
