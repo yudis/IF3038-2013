deef1bdc0b814d7ba6afbda4faa85febc669d9cc
Christianto Handojo <handojo.christianto@gmail.com>
2013-03-20 00:54:25 +0700

    Pembetulan Tambah Komentar
    Tambah komentar sudah terlihat dan masuk ke database, tapi belum ada
    scroll bar jika komen udah kebanyakan.

--- src/tambah_komentar.php
+++ src/tambah_komentar.php
@@ -9,8 +9,10 @@
     if (mysqli_connect_errno($con)) {
         echo "Gagal Connect";
     } else {
-        $query = "INSERT INTO komentar (username, id_tugas, waktu, isi) VALUES ('$user','$id_tugas','$waktu','$komentar')";
+        $query = "INSERT INTO komentar (username, id_tugas, waktu, isi)
+                  VALUES ('$user',$id_tugas,'$waktu',$komentar)";
         $result = mysqli_query($con,$query);
         echo $result;
+        mysqli_close($con);
     }
 ?>
