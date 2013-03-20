6d356bcb652cada051ae3e16c07c14f3ebc86d99
Muhammad Reza Mandala Putra <13509003@std.stei.itb.ac.id>
2013-03-20 18:42:48 +0700

    connectDB
    mysql_connect($host, $user, $pass) yang ada di login.php gw pindahin ke
    connectDB.php yaa
    biar bisa dipake di php lainnya juga

--- src/login.php
+++ src/login.php
@@ -2,18 +2,11 @@
 	$u = $_GET['u'];
 	$p = $_GET['p'];
 	
-	$host = 'localhost';
-	$user = 'progin';
-	$pass = 'progin';
-	$db = 'progin';
+	require 'connectDB.php';
 	
-	$con = mysql_connect($host, $user, $pass);
-	if(!$con) die("Could not connect: " . mysql_error());
-	
-	mysql_select_db($db, $con);
 	$query = "select username from login where username = '" . $u . "' and password = '" . $p . "'";
 	$result = mysql_query($query);
 	$row = mysql_fetch_array($result);
 	echo $row['username'];
-	mysql_close($con)
+	mysql_close($con);
 ?>
