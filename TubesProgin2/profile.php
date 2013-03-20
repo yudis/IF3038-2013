b01503dc9e0240aa54d900004ec49463459934eb
Martha Monica <13510080@std.stei.itb.ac.id>
2013-03-20 13:20:45 +0700

    inisialisasi awal

---
+++ TubesProgin2/profile.php
@@ -0,0 +1,54 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BANG! - Profile</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+        <script type="text/javascript" src="script.js"></script>
+    </head>
+    <body>
+        <header>
+            <a href="dashboard.php" title="Home"><img id="logo-small" src="img/Logo_Small2.png" alt="" /></a>
+            <div id="dashboard"><a title="Go to Dashboard" href="dashboard.php">Dashboard</a></div>
+            <div id="profile"><a title="Go to Profile" href="profile.php">My Profile</a></div>
+            <div id="logout"><a title="Log out from here" href="index.php">Log Out</a></div>
+            <form id="search">
+                <input type="text" name="Search" id="box">
+                <input type="submit" value="Search">
+            </form>
+        </header>
+        <div id="panel"></div>
+        <div id="donelist">
+            Ridho Ramadan<br/>
+            Daniel Ginting<br/>
+            Timotius Nugraha<br/>
+            Nicolas Rio<br/>
+            Aditya Agung
+        </div>
+        <div id="todolist">
+            Afif Alhawari<br/>
+            Flora Monica <br/>
+            Nugroho Satrijandi<br/>
+        </div>
+        <div id="biodata">
+            <img id="foto" src="img/foto_anonim.png">
+            <img id="badge" src="img/badge.png">
+            <div id="biousername"><script>document.write(localStorage.username);</script></div>
+            <div id="bioleft">
+                Name<br>
+                Date of Birth<br>
+                Email<br>
+            </div>
+            <div id="bioright">
+                : <script>document.write(localStorage.name);</script><br>
+                : <script>document.write(localStorage.date);</script><br>
+                : <script>document.write(localStorage.email);</script><br>
+            </div>
+        </div>
+        
+    </body>
+</html>
