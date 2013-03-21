d83c7bb7ca40cb2dd170919fcd1c9a2e9514f46d
anpandu <anpandumail@gmail.com>
2013-03-21 14:47:44 +0700

    header dan footer

---
+++ src/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+	Copyright © Luthfi - Fandi - Ananta
+</div>
---
+++ src/header.php
@@ -0,0 +1,31 @@
+<div class="header">
+  <div id="logo">
+	  <a hreg="dashboard.html">
+	  <img src="pict/logo.png">
+	  </a>
+  </div>
+  <div id="border">
+	  
+  </div>
+  <div id="dashboard">
+	  <a href="dashboard.html">DASHBOARD</a>
+  </div>
+  <div id="profile">
+	  <a href="profile.html">PROFILE</a>
+  </div>
+  <div id="search">
+	  <section class="searchform cf">
+		  <input class="searchbox" type="search" name="search" placeholder="Search.." required>
+		  
+	  </section>
+	  <section class="searchbuttonbox cf">
+		  <img class="searchbutton"src="pict/search button.jpg">
+	  </section>	  
+  </div>  
+  <div id="wellfaiz">
+	  
+  </div>
+  <div id="logout">
+	  <a href="index.html">LOGOUT</a>
+  </div>
+</div>
