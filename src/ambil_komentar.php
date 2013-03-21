aa21fb9f72fee1bb75836fea49f7d2bbfc90ed44
Christianto Handojo <handojo.christianto@gmail.com>
2013-03-21 19:45:44 +0700

    Perbaikan tambah komentar, pembuatan ambil komentar
    ambil komentar -> pas page load pertama kali, ambil data komentar yang
    ada di database.
    tambah komentar diperbaiki karena ada bug (salah indeks) sedikit.

---
+++ src/ambil_komentar.php
@@ -0,0 +1,27 @@
+<?php
+
+    $con = mysqli_connect("localhost","root","","progin_405_13510003");
+    
+    if (mysqli_connect_errno($con)) {
+        echo "";
+    } else {
+        $id_tugas = $_REQUEST["id_tugas"];
+    
+        if ($stmt = mysqli_prepare($con, "SELECT * FROM komentar WHERE (id_tugas = ?) ORDER BY waktu")) {
+            mysqli_stmt_bind_param($stmt,"d",$id_tugas);
+            mysqli_stmt_execute($stmt);
+            $h1="";
+            $h2=0;
+            $h3="";
+            $h4="";
+            mysqli_stmt_bind_result($stmt,$h1,$h2,$h3,$h4);
+                      
+            while (mysqli_stmt_fetch($stmt)) {
+                echo $h1,"\n",$h3,"\n",$h4,"\n";
+            }
+            
+            mysqli_stmt_close($stmt);
+        }        
+        mysqli_close($con);
+    }
+?>
--- src/tambah_komentar.php
+++ src/tambah_komentar.php
@@ -1,18 +1,20 @@
 <?php
-    $user = $_REQUEST["user"];
-    $id_tugas = $_REQUEST["id_tugas"];
-    $komentar = $_REQUEST["komentar"];
-    $waktu = date('Y-m-d H:i:s');
-    
     $con = mysqli_connect("localhost","root","","progin_405_13510003");
     
     if (mysqli_connect_errno($con)) {
         echo "Gagal Connect";
     } else {
-        $query = "INSERT INTO komentar (username, id_tugas, waktu, isi)
-                  VALUES ('$user',$id_tugas,'$waktu',$komentar)";
-        $result = mysqli_query($con,$query);
-        echo $result;
+        $user = $_REQUEST["user"];
+        $id_tugas = $_REQUEST["id_tugas"];
+        $waktu = date('Y-m-d H:i:s');
+        $komentar = $_REQUEST["komentar"];
+        
+        $stmt = mysqli_prepare($con, "INSERT INTO komentar (username, id_tugas, waktu, isi) VALUES (?,?,?,?)");
+        mysqli_stmt_bind_param($stmt,"sdss",$user,$id_tugas,$waktu,$komentar);
+        mysqli_stmt_execute($stmt);
+        echo mysqli_stmt_affected_rows($stmt);
+        mysqli_stmt_close($stmt);
+        
         mysqli_close($con);
     }
 ?>
