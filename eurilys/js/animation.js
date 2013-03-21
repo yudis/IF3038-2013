a3edb4b49489fa3b42de38cd986b90ed1b3a6c49
Sharon Loh <lohh.sharon@gmail.com>
2013-03-21 13:12:28 +0700

    Confirmation before finishTask()

--- eurilys/js/animation.js
+++ eurilys/js/animation.js
@@ -262,20 +262,24 @@ function generateTask(category) {
 }
 
 function finishTask(i) {
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	}
-	else {// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			document.getElementById("dynamic_content").innerHTML = xmlhttp.responseText;
+	var finishTaskConfirm = confirm("Are you sure this task has been completely done?");
+	if (finishTaskConfirm == true)
+	{
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
 		}
-	}	
-	xmlhttp.open("GET","dashboard_dynamic_content.php?q=all&taskdone="+i+"&taskid="+i,true);
-	xmlhttp.send();
+		else {// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.onreadystatechange=function() {
+			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+				document.getElementById("dynamic_content").innerHTML = xmlhttp.responseText;
+			}
+		}	
+		xmlhttp.open("GET","dashboard_dynamic_content.php?q=all&taskdone="+i+"&taskid="+i,true);
+		xmlhttp.send();
+	}
 }
 
 function viewTask(i) {
