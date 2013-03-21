573d3ae37aa19abe55882403a42b0870891573f2
Arya R. Viciyudha <arya.rezavidi@gmail.com>
2013-03-21 07:18:19 +0700

    belum bisa upload file, karena permission denied

--- src/post.php
+++ src/post.php
@@ -1,8 +1,11 @@
 <?php 
 include 'header.php';
 
-$id_cat = $_GET['id'];
+
+$id_cat= $_GET['cat'];
+
 ?>
+
 
 <div id="main">
 	<div id="konten">
@@ -13,37 +16,43 @@ $id_cat = $_GET['id'];
 				Post Tugas
 			</div>
 			<div class="isi">
-				<form action="" method="get">
-					<div class="register-label">Judul</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="judul" id="judul" onkeyup = "validatePostTask()" maxlength="25"/><div><label id="judulvalidate"> </label></div></div>
+				<form action="task.php" method="post" enctype="multipart/form-data">
+                	<input type="hidden" name="id" value="<?php echo $id_cat;?>">
+					<div class="register-label">Judul</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="judul" id="judul" onkeyup = "validatePostTask()" maxlength="25"/>
+                    <div><label id="judulvalidate"> </label></div></div>
 					<div class="clear error" id="error-username"></div>
-					<div class="register-label">Jenis konten</div><div class="register-td">:</div><div class="register-input"><input type="radio" name="sex" id="sex-link" value="link" onclick="linkClicked();" /> Link <input type="radio" name="sex" id="sex-gambar" value="gambar" onclick="gambarClicked();" /> Gambar <input type="radio" name="sex" id="sex-video" value="video" onclick="videoClicked();" /> Video</div>
+					
+                    
+                    
+                    <div class="register-label">Jenis konten</div><div class="register-td">:</div><div class="register-input">
+                        <input type="radio" name="file" id="tipe" value="file"  /> File 
+                        <input type="radio" name="file" id="tipe" value="image" /> Gambar 
+                        <input type="radio" name="file" id="tipe" value="video" /> Video</div>
 					<div class="clear" id="error-sex"></div>
 					<div id="opt"></div>
 					
-					<div id="artikel">
-						<div class="register-label">URL artikel</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="artikel" id="kontenartikel"/></div>
-						<div class="clear error" id="error-artikel"></div>
-						<div class="register-label">Deskripsi singkat</div><div class="register-td">:</div><div class="register-input"><textarea class="register-input-input" name="about" id="kontendeskripsi" rows="5" cols="25"></textarea></div>
-						<div class="clear"></div>
-					</div>
-					<div id="avatar">
-						<div class="register-label">File gambar</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="file" name="avatar" id="kontenavatar" /></div>
+					<div>
+						<div class="register-label">File upload</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="file" name="xxx" id="kontenavatar" />
+                        </div>
 						<div class="clear" id="error-gambar"></div>
 					</div>
-					<div id="youtube">
-						<div class="register-label">URL YouTube</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="youtube" id="kontenyoutube" /><br />
-						Contoh : https://www.youtube.com/watch?v=7oqsc6ahHVI</div>
-						<div class="clear error" id="error-youtube"></div>
-					</div>
-					
-					<div class="register-label">Deadline</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="tgl" id="form-tgl" onkeyup="validate()" /></div><div id="caldad"><div id="calendar"></div><a href="javascript:showcal(2,2012);void(0);"><img src="images/cal.gif" alt="Calendar" /></a></div>
+                    
+                    
+                    
+                    
+					<div class="register-label">Deadline</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="deadline" id="form-tgl" onkeyup="validate()" /></div><div id="caldad"><div id="calendar"></div><a href="javascript:showcal(2,2012);void(0);"><img src="images/cal.gif" alt="Calendar" /></a></div>
+                    
 					<div class="clear" id="error-tgl"></div>
+                    
 					<div class="register-label">Asignee</div><div class="register-td">:</div><div class="register-input"><input class="register-input-input" type="text" name="asignee" id="asignee"/></div>
+                    
 					<div class="clear error" id="error-username"></div>
-					<div class="register-label">Tag</div><div class="register-td">:</div><div class="register-input"><input class="registerinput-input"- type="text" name="judul" id="judul"/><ul><div><a href="profil.php"></a></div>
+                    
+					<div class="register-label">Tag</div><div class="register-td">:</div>
+                    <div class="register-input"><input class="registerinput-input"- type="text" name="tag" id="judul"/><ul><div><a href="profil.php"></a></div>
 					</ul></div>
 												
-					<div class="post-register-submit"><input type="button" id = "post1" name="post" value="OK" onclick="postContent()" disabled/></div>
+					<div class="post-register-submit"><input type="submit" id = "post1" name="post" value="OK" /></div>
 				</form>
 			</div>
 			
