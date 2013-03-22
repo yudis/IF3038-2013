fe3605cfd6a724992edfaf9e599d6fbaf9c11ea0
Abraham Krisnanda S <13510033@std.stei.itb.ac.id>
2013-03-22 17:35:40 +0700

    on the way new Task

---
+++ src/js/checker_edit_pass.js
@@ -0,0 +1,38 @@
+Rp(function() {
+	checkValidity = function() {
+		pattern = new RegExp(this.getAttribute('pattern'), 'i');
+		match = this.value.match(pattern);
+
+		diff = true;
+		if (neqe = this.getAttribute('data-neq')) {
+			ne = Rp('#' + neqe);
+			if (ne.val() == this.value)
+				diff = false;
+		}
+
+		if ((match && diff) || !this.value) {
+			// bener
+			Rp(this).removeClass('invalid');
+
+			inv = Rp('input[pattern].invalid');
+			if (this.value) {
+				Rp('#edit_submit').prop('disabled', (inv.length() > 0));
+			}
+		}
+		else {
+			// ga valid
+			console.log('Invalid: ' + this.id);
+			Rp(this).addClass('invalid');
+			Rp('#edit_submit').prop('disabled', true);
+		}
+	};
+
+	Rp('input[pattern]').on('keyup', checkValidity);
+	Rp('#edit_submit').prop('disabled', true); // disabled by default
+	
+	var edit_new_password = document.getElementById("edit_new_password");
+	edit_new_password.onkeyup = function()
+	{
+		document.getElementById("edit_confirm_password").pattern = this.value;
+	}
+});
