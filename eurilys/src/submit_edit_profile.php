638c21dc6855511b40fa172ac9534e9165ad0410
Nurul Fithria Lubis <nfithrialubis@gmail.com>
2013-03-22 21:01:09 +0700

    connect edit form to database
    avatar processing not yet completed

--- eurilys/src/submit_edit_profile.php
+++ eurilys/src/submit_edit_profile.php
@@ -8,6 +8,7 @@
 	$user        =    'root';
 	$password    =    '';
 	$database    =    'progin_405_13510086';
+	$ret		 = 1;
 
 	$con        =    mysql_connect($host,$user,$password) or die('Server information is not correct.');
 	mysql_select_db($database,$con) or die('Database information is not correct');
@@ -17,25 +18,59 @@
 	
 	if (isset($_POST['edit_profile_submit'])) { //when submit button button is pressed
 
-		
-
-		$fullname = mysql_real_escape_string($_POST['fullname']);
-		$birthdate = mysql_real_escape_string($_POST['birthdate']);
-		$avatar = mysql_real_escape_string($_POST['avatar']);
-		$password = mysql_real_escape_string($_POST['password']);	
-		$passwordconf = mysql_real_escape_string($_POST['password_confirm']);	
-
-
-		if ($password == $passwordconf) {
-
+		//process full name
+		if($_POST['fullname'] !== null) {
+			//edit user's full name
+			$fullname = mysql_real_escape_string($_POST['fullname']);
+			$query = "UPDATE user SET full_name='$fullname' WHERE username='$username'";
+			if (mysql_query($query)) {
+				mysql_query($query);
+			} else {
+				$ret = 0;
+			}
 		}
-		
-		
-		$query = "UPDATE user SET column1=value, column2=value2,... WHERE username='$username'"
 
+		//process birthdate
+		if($_POST['birthdate'] !== null) {
+			//edit user's birthdate
+			$birthdate = mysql_real_escape_string($_POST['birthdate']);
+			$query = "UPDATE user SET birthdate='$birthdate' WHERE username='$username'";
+			if (mysql_query($query)) {
+				//mysql_query($query);				
+			} else {
+				$ret = 0;
+			}
 		}
+
+		/*//process avatar
+		if($_POST['avatar'] !== null) {
+			//edit user's avatar
+			$avatar = mysql_real_escape_string($_POST['avatar']);
+			$query = "UPDATE user SET avatar='$avatar' WHERE username='$username'";
+			if (mysql_query($query)) {
+				mysql_query($query);				
+			} else {
+				$ret = 0;
+			}
+		}*/
+
+		if($_POST['password'] !== "") {
+			//edit user's password
+			//checking password confirmation is not necessary as it's done in javascript
+			$password = mysql_real_escape_string($_POST['password']);
+			$query = "UPDATE user SET password='$password' WHERE username='$username'";
+			if (mysql_query($query)) {
+				mysql_query($query);				
+			} else {
+				$ret = 0;
+			}
+		}
+	
+		
+			
 
 	}
+
 
 
 
