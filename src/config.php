8499daa8bfe876df7052fdac6211dd4bf3dc01a7
Muhammad Wachid Kusuma <wachidx@gmail.com>
2013-03-22 09:37:58 +0700

    database changes

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "progin";
+$mysql_user = "progin";
+$mysql_password = "progin";
+$mysql_database = "progin_405_13510074";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
 mysql_select_db($mysql_database, $bd) or die("Opps some thing went wrong");
 ?>
