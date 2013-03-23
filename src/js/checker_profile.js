24104332af11b2c5a01c97b47bb456b89ec4c7f6
Jordan Fernando <fernandojordan.92@gmail.com>
2013-03-23 16:53:25 +0700

    add checker profile

--- src/js/checker_profile.js
+++ src/js/checker_profile.js
@@ -1,35 +1,77 @@
-Rp(function() {
-	/*checkValidity = function() {
-		pattern = new RegExp(this.getAttribute('pattern'), 'i');
-		match = this.value.match(pattern);
-
-		diff = true;
-		if (neqe = this.getAttribute('data-neq')) {
-			ne = Rp('#' + neqe);
-			if (ne.val() == this.value)
-				diff = false;
+Rp(function() {	
+	var profil_name = document.getElementById("edit_name");
+	var birth_date = document.getElementById("birth_date");
+	var profil_email = document.getElementById("edit_email");
+	var profil_avatar = document.getElementById("edit_avatar");
+	
+	profil_name.onkeyup = function()
+	{
+		if (this.checkValidity())
+			this.className = "";
+		else
+			this.className = "invalid";
+		check_submit();
+	}
+	
+	birth_date.onkeyup = function()
+	{
+		if ((this.checkValidity()) && (check_date(this.value)))
+			this.className = "";
+		else
+			this.className = "invalid";
+		check_submit();
+	}
+	
+	profil_email.onkeyup = function()
+	{
+		if ((this.checkValidity()))
+			this.className = "";
+		else
+			this.className = "invalid";
+		check_submit();
+	}
+	
+	profil_avatar.onchange = function()
+	{
+		if (check_file_jpeg(this))
+			this.className = "";
+		else
+			this.className = "invalid";
+		check_submit();
+	}
+	
+	function check_date(date)
+	{
+		var temp = date.split("-");
+		var d = new Date(parseInt(temp[0]), (parseInt(temp[1]) - 1), parseInt(temp[2]));
+		if ((d) && ((d.getMonth() + 1) == parseInt(temp[1])) && (d.getDate() == Number(parseInt(temp[2]))) && (parseInt(temp[0]) >= 1955))
+		{
+			datePicker.populateTable(d.getMonth(),d.getFullYear());
+			return true;
 		}
-
-		if ((match && diff) || !this.value) {
-			// bener
-			Rp(this).removeClass('invalid');
-
-			inv = Rp('input[pattern].invalid');
-			if (this.value) {
-				Rp('#edit_submit').prop('disabled', (inv.length() > 0));
-			}
+		else
+			return false;
+	}
+	
+	function check_file_jpeg(image)
+	{
+		return (image.value.match("^.+\.(jpe?g|JPE?G)$"));
+	}
+	
+	function check_submit()
+	{
+		if ((profil_name.checkValidity()) && (birth_date.checkValidity()) && 
+			(profil_email.checkValidity()) && ((profil_avatar.value=="")||(check_file_jpeg(profil_avatar))) &&
+			(check_date(birth_date.value)))
+		{
+			document.getElementById("edit_submit").disabled="";
 		}
-		else {
-			// ga valid
-			console.log('Invalid: ' + this.id);
-			Rp(this).addClass('invalid');
-			Rp('#edit_submit').prop('disabled', true);
+		else
+		{
+			document.getElementById("edit_submit").disabled="disabled";
 		}
-	};
-
-	Rp('input[pattern]').on('keyup', checkValidity);
-	Rp('#edit_submit').prop('disabled', true); // disabled by default
-	*/
+	}
+	
 	window.onload = function() 
 	{
 		datePicker.init(document.getElementById("calendar"), document.getElementById("edit_form"), "birth_date");
