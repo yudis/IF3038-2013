b9399f8ec967ca208d24143188e168f8481a9f34
Christianto Handojo <handojo.christianto@gmail.com>
2013-03-23 11:20:57 +0700

    Hapus komentar dan paginasi
    TODO: Attachment, add task.

--- src/ambil_komentar.php
+++ src/ambil_komentar.php
@@ -5,9 +5,10 @@
         echo "Gagal Koneksi";
     } else {
         $id_tugas = $_REQUEST["id_tugas"];
+        $mulai = 10 * ($_REQUEST["start"]-1);
     
-        if ($stmt = mysqli_prepare($con, "SELECT * FROM komentar WHERE (id_tugas = ?) ORDER BY waktu")) {
-            mysqli_stmt_bind_param($stmt,"d",$id_tugas);
+        if ($stmt = mysqli_prepare($con, "SELECT * FROM komentar WHERE (id_tugas = ?) ORDER BY waktu LIMIT ?,10")) {
+            mysqli_stmt_bind_param($stmt,"dd",$id_tugas,$mulai);
             mysqli_stmt_execute($stmt);
             $h1="";
             $h2=0;
@@ -15,7 +16,7 @@
             $h4="";
             mysqli_stmt_bind_result($stmt,$h1,$h2,$h3,$h4);
             
-            $array;
+            $array=array();
             
             while (mysqli_stmt_fetch($stmt)) {
                 $array[] = array($h1,$h3,$h4);
@@ -24,7 +25,7 @@
             mysqli_stmt_close($stmt);
             
             foreach ($array as $row) {
-                $result = mysqli_query($con,"SELECT avatar FROM pengguna WHERE (username = '$h1')");
+                $result = mysqli_query($con,"SELECT avatar FROM pengguna WHERE (username = '$row[0]')");
                 $avatar = mysqli_fetch_row($result);
                 echo $row[0],"\n",$row[1],"\n",$row[2],"\n",$avatar[0],"\n";
                 mysqli_free_result($result);
