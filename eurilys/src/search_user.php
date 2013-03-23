ae6f444a6e598ca065ba8cb5fb96e5318bf07df2
Nurul Fithria Lubis <nfithrialubis@gmail.com>
2013-03-23 21:47:32 +0700

    fix user's avatar at search result

--- eurilys/src/search_user.php
+++ eurilys/src/search_user.php
@@ -24,7 +24,7 @@
 	
 	//Set the search result at response
 	if (strlen($q) > 0) {		
-		$response = "<div class='half_div'><div id='upperprof'><img src='../img/avatar1.png' alt=''><div id='namauser'>".$fullname."</div></div> <br/><br/></div>";
+		$response = "<div class='half_div'><div id='upperprof'><img id='mainpp' width='225' src='$avatar' alt=''><div id='namauser'>".$fullname."</div></div> <br/><br/></div>";
 		$response = $response ."<div class='half_div'> <div class='user_search_result_label'> Username  </div> <div class='left'> ".$username." </div> <div class='clear'></div> <br>";
 		$response = $response ."<div class='user_search_result_label'> Email </div><div class='left'> ".$email." </div><div class='clear'></div><br>";	
 		$response = $response . "<div class='user_search_result_label'>  Birthdate  </div> <div class='left'> ".$birthdate." </div><div class='clear'></div></div>";
