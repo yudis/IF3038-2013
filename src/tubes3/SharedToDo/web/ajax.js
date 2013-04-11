3ebaaa01befc877e0f5ce6e3c9498d8b6cf53013
Muhammad Reza Mandala Putra <13509003@std.stei.itb.ac.id>
2013-04-11 23:13:58 +0700

    commit semoga bener
    commit semoga bener

--- src/tubes3/SharedToDo/web/ajax.js
+++ src/tubes3/SharedToDo/web/ajax.js
@@ -1,19 +1,30 @@
 function createAJAX()
 {
 	var xmlhttp;
-	if(window.XMLHttpRequest){
-		xmlhttp = new XMLHttpRequest();
-	}
-	else{
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	return xmlhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else 
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    return xmlhttp;
 }
 
 function postAJAX(xmlhttp, url, parameters)
 {
-	xmlhttp.open("POST", url, true);
-	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xmlhttp.send(parameters);
+	xmlhttp.open("POST", url + "?" + parameters, true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send();
+    //xmlhttp.send(parameters);
+}
+
+function postAJAX2(xmlhttp, url, header, parameters)
+{
+    xmlhttp.open("POST", url + "?" + parameters, true);
+    xmlhttp.setRequestHeader("Content-type", header);
+    xmlhttp.send();
+    //xmlhttp.send(parameters);
 }
