8931f3b1ef505b4ba9bb9f04a1f139d0b988e7e0
meckyr <ecky_dozha@yahoo.co.id>
2013-04-12 21:43:58 -0700

    autocomplete edit assignee di task page

--- TaskSharedWeb/web/javascript/task_page.js
+++ TaskSharedWeb/web/javascript/task_page.js
@@ -162,20 +162,28 @@ function autoCompleteAsignee() {
 	getAjax();
 
 	var asignee = document.getElementById("task-assignee").value;
+	var asigneeindex;
+        var asigneearray;
 	var suggestion = "";
 	var suggestionarray;
 	
 	var index = asignee.length;
-	
+        
 	if(asignee!="")
 	{
-		if (asignee.charAt(index - 1) == ',')
-		{
-			konkat = asignee.substr(0,index);
-		}
-		
-		ajaxRequest.open("GET","autocompleteasignee?asignee="+document.getElementById("task-assignee").value,false);
-
+		if (asignee.indexOf(",") !== -1) {
+                    if (asignee.charAt(index - 1) === ',')
+                    {
+                            konkat = asignee.substr(0,index);
+                    }
+                    asigneearray = asignee.split(",");
+                    asigneeindex = asigneearray.length - 1;
+                    ajaxRequest.open("GET","autocompleteasignee?asignee="+asigneearray[asigneeindex],false);
+                }
+                else {
+                    ajaxRequest.open("GET","autocompleteasignee?asignee="+asignee,false);
+                }
+                
 		ajaxRequest.onreadystatechange = function()
 		{
 			suggestion =  ajaxRequest.responseText;
