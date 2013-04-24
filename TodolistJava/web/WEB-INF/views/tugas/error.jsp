a6b1177a5dc23f02d1ee754e832e6c7911dad52f
Edward Samuel <edward@edward-K42Jc.(none)>
2013-04-24 13:55:51 +0700

    Start Tubes 4

--- TodolistJava/web/WEB-INF/views/tugas/error.jsp
+++
@@ -1,5 +0,0 @@
-<jsp:include page="/WEB-INF/includes/header.inc.jsp" />
-                <h1>Maaf, tugas tidak ditemukan atau telah dihapus.</h1>
-                <p class="padding12px">Tugas dengan <strong>ID = <%= request.getParameter("id") %></strong> tidak ditemukan.</p>
-                <br />
-<jsp:include page="/WEB-INF/includes/footer.inc.jsp" />
--- TodolistPHP/includes/footer.inc.php
+++
@@ -1,9 +0,0 @@
-            </div>
-            <footer class="content" id="footerpage">
-                This website is created solely for the purpose of fulfilling our college task.<br />
-                IF3094 - Pemrograman Internet.<br />
-                &copy; 2013 Ranger Enemy - Edward Samuel Pasaribu, Felix Terahadi, Wilson Fonda
-            </footer>
-        </div>
-    </body>
-</html>
