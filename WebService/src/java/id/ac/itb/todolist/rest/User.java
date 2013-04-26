61091ae4aadbcb35e1e6eaf32c22052879ecb607
raymond <raymond_pluto@hotmail.com>
2013-04-26 14:05:46 +0700

    memperbaiki project

---
+++ WebService/src/java/id/ac/itb/todolist/rest/User.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.itb.todolist.rest;
+
+import id.ac.itb.todolist.dao.UserDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Raymond
+ */
+public class User extends HttpServlet {
+
+    private Pattern regexUpdate = Pattern.compile("^/([\\w._%].*)/([\\w._%].*)$");
+    private Pattern regexUserDetail = Pattern.compile("^/([\\w._%].*)$");
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        String pathInfo = request.getPathInfo();
+        Matcher matcher;
+
+        matcher = regexUserDetail.matcher(pathInfo);
+        if (matcher.find()) {
+            UserDao userDao = new UserDao();
+            out.print(userDao.getUser(matcher.group(1)).toJsonObject());
+            return;
+        }
+
+        throw new ServletException("Invalid URI");
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
