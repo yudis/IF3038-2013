b54d47b0b5e200428628c5ebf722c6df079d4ad6
fmuhtadin <fmuhtadin@gmail.com>
2013-05-01 13:30:00 +0700

    Commit

---
+++ progin4solasido/src/com/progin4solasido/server/PMF.java
@@ -0,0 +1,15 @@
+package com.progin4solasido.server;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+public final class PMF {
+    private static final PersistenceManagerFactory pmfInstance =
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+
+    private PMF() {}
+
+    public static PersistenceManagerFactory get() {
+        return pmfInstance;
+    }
+}
