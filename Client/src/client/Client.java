39ef5e4420fe83899631b441ce1482927d16baff
Chlorophyne <13510056@std.stei.itb.ac.id>
2013-05-18 19:38:38 +0700

    Simple Server and Multi Client connection have been implemented

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -3,10 +3,11 @@
  * and open the template in the editor.
  */
 package client;
-
+import java.io.*;
+import java.net.*;
 /**
  *
- * @author Compaq
+ * @author Timotius
  */
 public class Client {
 
@@ -14,6 +15,26 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String command;
+        String message;
+        Boolean running;
+        
+        try{
+            running = true;
+            BufferedReader inClient = new BufferedReader( new InputStreamReader(System.in));
+            Socket clientSocket = new Socket("localhost", 1234);
+            DataOutputStream outServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while (running){
+                System.out.print(">> ");
+                command = inClient.readLine();
+                outServer.writeBytes(command + '\n');
+                message = inServer.readLine();
+                System.out.println("From Server: " + message);
+            }
+            clientSocket.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -3,17 +3,58 @@
  * and open the template in the editor.
  */
 package server;
-
+import java.io.*;
+import java.net.*;
 /**
  *
- * @author Compaq
+ * @author Timotius
  */
-public class Server {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Server implements Runnable{
+    
+    protected static int port = 1234;
+    Socket connection;
+    private int ID;
+    private static int count = 0;
+    String message;
+    String capitalizedMessage;
+    
+    Server(Socket socket, int id) {
+        this.connection = socket;
+        this.ID = id;
+    }
+        
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try{
+            ServerSocket socket = new ServerSocket(port);
+            System.out.println("Server ready...");
+            while (true) {
+                Socket connection = socket.accept();
+                Runnable runnable = new Server(connection, ++count);
+                System.out.println("New connection with ID: "+count);
+                Thread thread = new Thread(runnable);
+                thread.start();
+            }
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void run() {
+        
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            while (true){
+                message = in.readLine();
+                System.out.println("From Client: " + message);
+                capitalizedMessage = message.toUpperCase() + '\n';
+                out.writeBytes(capitalizedMessage);
+                Thread.sleep(1000);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
