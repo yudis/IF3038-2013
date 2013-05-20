c2f4f6466a1ec86fd5effb80b66a43750c7284d8
Edward Samuel <edwardsamuel92@gmail.com>
2013-05-20 23:42:45 +0700

    Final Tubes V

--- 5-Offline/TodolistServer/src/id/ac/itb/todolist/util/ProxyUtil.java
+++ 5-Offline/TodolistServer/src/id/ac/itb/todolist/util/ProxyUtil.java
@@ -11,34 +11,34 @@ import java.util.Properties;
 public class ProxyUtil {
 
     private static Proxy proxy = null;
+
     public static Proxy getProxy() {
         try {
             Properties prop = new Properties();
             InputStream inputStream = new FileInputStream("proxy.properties");
             prop.load(inputStream);
-            
+
             String type = prop.getProperty("type");
             if ("DIRECT".equalsIgnoreCase(type)) {
                 proxy = Proxy.NO_PROXY;
             } else {
                 String hostname = prop.getProperty("hostname");
                 int port = Integer.parseInt(prop.getProperty("port"));
-                
+
                 if ("HTTP".equalsIgnoreCase(type)) {
                     proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(hostname, port));
                 } else if ("SOCKS".equalsIgnoreCase(type)) {
-                    proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(hostname, port));                    
+                    proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(hostname, port));
                 } else {
                     proxy = Proxy.NO_PROXY;
                     throw new IllegalArgumentException("Type doesn't supported, using direct proxy.");
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
+            proxy = Proxy.NO_PROXY;
             e.printStackTrace();
         }
-        
+
         return proxy;
     }
 }
