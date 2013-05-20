1a6453162d53e2d7fda299ea8208d6eac292f178
Aisyah Dzulqaidah <aisyahdzulqaidah@yahoo.co.id>
2013-05-21 02:15:43 +0700

    untuk connect sql

--- src/ProginV/src/client/ConnectSql.java
+++ src/ProginV/src/client/ConnectSql.java
@@ -4,10 +4,35 @@
  */
 package client;
 
+
+import java.sql.*;
 /**
  *
  * @author Aisyah
  */
 public class ConnectSql {
+    public static void main (String[] args){
+        String url ="jdbc:mysql://localhost:3306/";
+        String dbname = "progin";
+        String driver="com.mysql.jdbc.Driver";
+        String username= "root";
+        String password="";
+        
+        try{
+        Class.forName(driver).newInstance();
+        Connection conn = DriverManager.getConnection(url+dbname,username,password);
+        
+        conn.close();
+        }
+        catch (Exception e)
+        {
+        e.printStackTrace();
+        }
+        
+        }
     
-}
+    
+    }
+    
+    
+
