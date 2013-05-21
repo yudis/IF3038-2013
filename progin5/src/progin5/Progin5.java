1bd6b66567387bc45e5af294069dd688cde4f3af
fmuhtadin <fmuhtadin@gmail.com>
2013-05-21 08:48:14 +0700

    Commit

---
+++ progin5/src/progin5/Progin5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progin5;
+
+/**
+ *
+ * @author Compaq
+ */
+public class Progin5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
--- src/MEMBER.md
+++
@@ -1,4 +0,0 @@
-No	NIM			NAMA					MAIL					GITHUB_ID
-1	13510018	Arie Tando				arie.tando91@gmail.com	apek-chen
-2	13510070	Fadhil Muhtadin			fmuhtadin@gmail.com		fmuhtadin
-3	13510074	Muhammad Wachid Kusuma	wachidx@gmail.com		chidx
